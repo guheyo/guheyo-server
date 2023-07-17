baa2947813f3b74ba8a35fdbcc7af0ca8df14a54
YUNGU HAN <keypport@gmail.com>
2023-07-18 00:05:28 +0900

    style: add eslint, prettier config files

---
+++ .eslintrc.js
@@ -0,0 +1,65 @@
+const OFF = 0;
+const WARNING = 1;
+const ERROR = 2;
+
+module.exports = {
+  env: {
+    es2021: true,
+  },
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  extends: ['airbnb', 'airbnb-typescript', 'prettier'],
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': ERROR,
+    'import/prefer-default-export': OFF,
+    'no-nested-ternary': OFF,
+    'no-ternary': OFF,
+    'no-unneeded-ternary': WARNING,
+    'no-param-reassign': [ERROR, { props: false }],
+    'no-use-before-define': OFF,
+    'no-unused-vars': OFF,
+    '@typescript-eslint/no-use-before-define': ERROR,
+    '@typescript-eslint/no-unused-vars': [ERROR, { args: 'none' }],
+    '@typescript-eslint/naming-convention': [
+      ERROR,
+      {
+        selector: 'default',
+        format: ['camelCase'],
+      },
+      {
+        selector: 'typeLike',
+        format: ['PascalCase'],
+      },
+      {
+        selector: 'variable',
+        format: ['camelCase', 'UPPER_CASE'],
+      },
+      {
+        selector: 'objectLiteralProperty',
+        format: null,
+      },
+    ],
+    'check-file/filename-naming-convention': [
+      ERROR,
+      {
+        '**/*.{jsx,tsx}': 'KEBAB_CASE',
+        '**/*.{js,ts}': 'KEBAB_CASE',
+      },
+      {
+        ignoreMiddleExtensions: true,
+      },
+    ],
+    'check-file/folder-naming-convention': [
+      ERROR,
+      {
+        '*/**/': 'KEBAB_CASE',
+      },
+    ],
+  },
+};
