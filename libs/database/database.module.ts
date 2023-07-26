351509f41b26f500d46b66c4a4d889577197a0fd
왕승재 <essential2189@gmail.com>
2023-07-26 10:16:02 +0900

    fest: prisma & redis setting (#14)
    * chore: add yaml
    
    * feat: prisma & redis setting
    
    * fix: enableShutdownHooks 지원 중단
    
    * chore: downgrade prisma from 5.0.0 to 4.13.0 - migration 미적용
    
    * style: apply eslint and prettier
    
    * fix: resolve peer dependency error
    
    ---------
    
    Co-authored-by: keypport <keypport@gmail.com>

--- libs/database/database.module.ts
+++ libs/database/database.module.ts
@@ -1,13 +1,25 @@
 import { DynamicModule } from '@nestjs/common';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { ConfigService } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { RedisClientOptions } from 'redis';
+import { CacheModule } from '@nestjs/cache-manager';
+import { CacheModuleOptions } from '@nestjs/common/cache';
+import { PrismaService } from '@lib/database/prisma.service';
 
 export class DatabaseModule {
-  static typeOrmModule(database: string, options: TypeOrmModuleOptions): DynamicModule {
-    return TypeOrmModule.forRootAsync({
+  static prismaModule(): DynamicModule {
+    return {
+      module: DatabaseModule,
+      providers: [PrismaService],
+      exports: [PrismaService],
+    };
+  }
+
+  static redisModule(options: CacheModuleOptions): DynamicModule {
+    return CacheModule.registerAsync<RedisClientOptions>({
+      imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: async (configService: ConfigService): Promise<TypeOrmModuleOptions> => {
-        const config = configService.get(database);
+      useFactory: async (configService: ConfigService): Promise<CacheModuleOptions> => {
+        const config = configService.get('redis');
         return { ...config, ...options };
       },
     });
