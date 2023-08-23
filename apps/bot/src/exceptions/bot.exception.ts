7fa1ab769b9ff8d8ee72e079710f48ec8f9f4a7e
keypport <keypport@gmail.com>
2023-08-23 16:58:44 +0900

    feat: remove src files of api and bot

--- apps/bot/src/exceptions/bot.exception.ts
+++
@@ -1,9 +0,0 @@
-import { HttpException, HttpStatus, NotFoundException } from '@nestjs/common';
-import { HttpResponse } from '@lib/shared';
-
-export class BotException {
-  notFound(): HttpException {
-    const response = new HttpResponse(HttpStatus.NOT_FOUND, 'not found');
-    return new NotFoundException(response);
-  }
-}
