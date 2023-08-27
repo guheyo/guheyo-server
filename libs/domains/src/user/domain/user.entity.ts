89311dd94b7438520934a7fa0e61116a58262f23
keypport <113260136+keypport@users.noreply.github.com>
2023-08-28 06:27:10 +0900

    feat: create user domain entity classes (#24)
    * feat: add prisma-class-generator
    
    * feat: create entity classes in domain
    
    * chore: add @nestjs/swagger and @prisma/client in domains
    
    * feat: use @Exclude and @Type decorators

---
+++ libs/domains/src/user/domain/user.entity.ts
@@ -0,0 +1,43 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { User } from '@prisma/client';
+import { Exclude, Type } from 'class-transformer';
+import { SocialAccountEntity } from './social-account.entity';
+import { MemberEntity } from './member.entity';
+
+export class UserEntity implements User {
+  constructor(partial: Partial<UserEntity>) {
+    Object.assign(this, partial);
+  }
+
+  @ApiProperty({ type: String })
+  id: string;
+
+  @ApiProperty({ type: Date })
+  createdAt: Date;
+
+  @ApiProperty({ type: Date })
+  updatedAt: Date;
+
+  @Exclude()
+  deletedAt: Date | null;
+
+  @ApiPropertyOptional({ type: String })
+  name: string | null;
+
+  @ApiProperty({ type: String })
+  username: string;
+
+  @Type(() => SocialAccountEntity)
+  @ApiProperty({ isArray: true, type: () => SocialAccountEntity })
+  socialAccounts: SocialAccountEntity[];
+
+  @ApiPropertyOptional({ type: String })
+  avatarURL: string | null;
+
+  @ApiProperty({ type: Boolean })
+  bot: boolean;
+
+  @Type(() => MemberEntity)
+  @ApiProperty({ isArray: true, type: () => MemberEntity })
+  members: MemberEntity[];
+}
