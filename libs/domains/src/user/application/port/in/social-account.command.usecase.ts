317512a8015a76e92d30ff849ed269061e559291
keypport <keypport@gmail.com>
2023-08-28 07:11:06 +0900

    feat: remove duplicated parameter userId

--- libs/domains/src/user/application/port/in/social-account.command.usecase.ts
+++ libs/domains/src/user/application/port/in/social-account.command.usecase.ts
@@ -2,6 +2,6 @@ import { SessionCreateRequest } from './session.create.request';
 import { SessionResponse } from './session.response';
 
 export interface SocialAccountCommandUseCase {
-  create(userId: string, sessionCreateRequest: SessionCreateRequest): Promise<SessionResponse>;
+  create(sessionCreateRequest: SessionCreateRequest): Promise<SessionResponse>;
   delete(userId: string, provider: string, socialId: string): Promise<SessionResponse>;
 }
