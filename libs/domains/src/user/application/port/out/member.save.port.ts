89d61d20df37826e31d52640a54d0d5d3e7e8597
keypport <keypport@gmail.com>
2023-08-29 03:39:20 +0900

    feat: add member save port

--- libs/domains/src/user/application/port/out/member.save.port.ts
+++ libs/domains/src/user/application/port/out/member.save.port.ts
@@ -2,4 +2,6 @@ import { MemberEntity } from '@lib/domains/user/domain/member.entity';
 
 export interface MemberSavePort {
   create(member: MemberEntity): Promise<MemberEntity>;
+  connectRoles(id: string, roleIds: string[]): Promise<MemberEntity>;
+  disconnectRoles(id: string, roleIds: string[]): Promise<MemberEntity>;
 }
