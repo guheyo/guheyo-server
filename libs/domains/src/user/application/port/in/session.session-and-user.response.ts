62b72dae4cc23d854de6ab3f01e7e9d044139d92
keypport <113260136+keypport@users.noreply.github.com>
2023-08-28 16:18:38 +0900

    feat: add an incoming port to the user application (#25)
    * feat: add user request and response
    
    * feat: add user command and query use cases
    
    * feat: add social-account request and reponse
    
    * feat: add social-account command use case
    
    * feat: add session request and response
    
    * feat: add session command and query use cases
    
    * feat: remove duplicated parameter userId
    
    * feat: add member request and reponse
    
    * feat: add member command use case
    
    * feat: unify parameter name to request in command use case

---
+++ libs/domains/src/user/application/port/in/session.session-and-user.response.ts
@@ -0,0 +1,8 @@
+import { SessionResponse } from './session.response';
+import { UserResponse } from './user.response';
+
+export class SessionAndUserResponse {
+  session: SessionResponse;
+
+  user: UserResponse;
+}
