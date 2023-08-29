92c11ccd99a621dc15074a2d5ff463e8ba4ea63c
keypport <113260136+keypport@users.noreply.github.com>
2023-08-30 06:33:30 +0900

    feat: separate member from user aggregate (#28)
    * fix: rename int to in
    
    * feat: separate member from user aggregate

--- libs/domains/src/member/application/port/out/member.save.port.ts
+++ libs/domains/src/member/application/port/out/member.save.port.ts
@@ -1,7 +1,8 @@
-import { MemberEntity } from '@lib/domains/user/domain/member.entity';
+import { MemberEntity } from '@lib/domains/member/domain/member.entity';
 
 export interface MemberSavePort {
   create(member: MemberEntity): Promise<MemberEntity>;
+  delete(userId: string, guildId: string): void;
   connectRoles(id: string, roleIds: string[]): Promise<MemberEntity>;
   disconnectRoles(id: string, roleIds: string[]): Promise<MemberEntity>;
 }
