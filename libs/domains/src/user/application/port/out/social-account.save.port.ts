23c04a86e5793de9ffd04fe77fa7c11a8550bb9c
keypport <keypport@gmail.com>
2023-09-02 08:28:29 +0900

    feat: change return type of save port to Promise<void>

--- libs/domains/src/user/application/port/out/social-account.save.port.ts
+++ libs/domains/src/user/application/port/out/social-account.save.port.ts
@@ -1,6 +1,7 @@
 import { SocialAccountEntity } from '@lib/domains/user/domain/social-account.entity';
 
 export interface SocialAccountSavePort {
-  create(socialAccount: SocialAccountEntity): Promise<SocialAccountEntity>;
-  delete(userId: string, provider: string, socialId: string): Promise<SocialAccountEntity>;
+  create(socialAccount: SocialAccountEntity): Promise<void>;
+  update(socialAccount: SocialAccountEntity): Promise<void>;
+  delete(userId: string, provider: string, socialId: string): Promise<void>;
 }
