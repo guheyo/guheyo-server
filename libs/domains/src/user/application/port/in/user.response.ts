3da991567545c4e5ac79690957e70fb85c1e732c
keypport <113260136+keypport@users.noreply.github.com>
2023-09-03 22:55:04 +0900

    feat: add user query and handler (#31)
    * feat: rename commands folder to command
    
    * feat: change user query usecase to user query and handler
    
    * feat: add tests for user handlers
    
    * feat: delete social account usecase and request
    
    * fix: change ICommand to IQuery
    
    * feat: delete social-account, user response
    
    * feat: change return type from UserResponse to UserEntity

--- libs/domains/src/user/application/port/in/user.response.ts
+++
@@ -1,4 +0,0 @@
-import { OmitType } from '@nestjs/swagger';
-import { UserEntity } from '@lib/domains/user/domain/user.entity';
-
-export class UserResponse extends OmitType(UserEntity, ['updatedAt', 'deletedAt'] as const) {}
