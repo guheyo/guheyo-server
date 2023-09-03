67de8a5a72de7cd8f369b570aa4ea3604530b47f
keypport <113260136+keypport@users.noreply.github.com>
2023-09-03 22:55:04 +0900

    feat: add user query and handler (#31)
    * feat: rename commands folder to command
    
    * feat: change user query usecase to user query and handler
    
    * feat: add tests for user handlers
    
    * feat: delete social account usecase and request
    
    * fix: change ICommand to IQuery
    
    * feat: delete social-account, user response
    
    * feat: change return type from UserResponse to UserEntity

--- libs/domains/src/user/application/port/in/social-account.command.usecase.ts
+++
@@ -1,7 +0,0 @@
-import { SocialAccountCreateRequest } from './social-account.create.request';
-import { SocialAccountResponse } from './social-account.response';
-
-export interface SocialAccountCommandUseCase {
-  create(request: SocialAccountCreateRequest): Promise<SocialAccountResponse>;
-  delete(userId: string, provider: string, socialId: string): Promise<SocialAccountResponse>;
-}
