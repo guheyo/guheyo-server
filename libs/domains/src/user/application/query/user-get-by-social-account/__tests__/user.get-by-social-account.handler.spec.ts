d75728eb5cc992aa87b2c29765b6d2947ed67f41
keypport <113260136+keypport@users.noreply.github.com>
2023-09-03 22:55:04 +0900

    feat: add user query and handler (#31)
    * feat: rename commands folder to command
    
    * feat: change user query usecase to user query and handler
    
    * feat: add tests for user handlers
    
    * feat: delete social account usecase and request
    
    * fix: change ICommand to IQuery
    
    * feat: delete social-account, user response
    
    * feat: change return type from UserResponse to UserEntity

---
+++ libs/domains/src/user/application/query/user-get-by-social-account/__tests__/user.get-by-social-account.handler.spec.ts
@@ -0,0 +1,32 @@
+import { Test } from '@nestjs/testing';
+import { mock, verify, instance } from 'ts-mockito';
+import { UserQueryAdapter } from '@lib/domains/user/adapter/out/persistence/user.query.adapter';
+import { UserLoadPort } from '../../../port/out/user.load.port';
+import { UserGetBySocialAccountQuery } from '../user.get-by-social-account.query';
+import { UserGetBySocialAccountHandler } from '../user.get-by-social-account.handler';
+
+describe('UserGetBySocialAccountQuery', () => {
+  let handler: UserGetBySocialAccountHandler;
+  const loadPort: UserLoadPort = mock(UserQueryAdapter);
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      providers: [
+        UserGetBySocialAccountHandler,
+        {
+          provide: 'UserLoadPort',
+          useValue: instance(loadPort),
+        },
+      ],
+    }).compile();
+    handler = moduleRef.get<UserGetBySocialAccountHandler>(UserGetBySocialAccountHandler);
+  });
+
+  describe('execute', () => {
+    it('should execute getBySocailAccount', async () => {
+      const query: UserGetBySocialAccountQuery = { provider: 'discord', socialId: 'social-id' };
+      await handler.execute(query);
+      verify(loadPort.getBySocailAccount(query.provider, query.socialId)).once();
+    });
+  });
+});
