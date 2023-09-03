8b15b47719ac64e217cc079cb51524e48b199bec
keypport <keypport@gmail.com>
2023-09-03 22:42:59 +0900

    feat: delete social-account, user response

--- libs/domains/src/user/application/query/response/social-account.response.ts
+++
@@ -1,8 +0,0 @@
-import { OmitType } from '@nestjs/swagger';
-import { SocialAccountEntity } from '@lib/domains/user/domain/social-account.entity';
-
-export class SocialAccountResponse extends OmitType(SocialAccountEntity, [
-  'id',
-  'updatedAt',
-  'deletedAt',
-] as const) {}
