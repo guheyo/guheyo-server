5f84796dcd6fa847ae0fdd42703020f2be6139ea
keypport <113260136+keypport@users.noreply.github.com>
2023-09-04 05:49:12 +0900

    feat: add social-account command and handler  (#32)
    * fix: change return type to Promise<void>
    
    * feat: return with new UserEntity
    
    * feat: add social account command adapter
    
    * feat: move command files to each command folder
    
    * feat: add social-account create handler
    
    * feat: add social-account update handler
    
    * feat: add social-account delete handler

---
+++ libs/domains/src/user/application/command/social-account-create/__tests__/social-account.create.handler.spec.ts
@@ -0,0 +1,45 @@
+import { Test } from '@nestjs/testing';
+import { mock, verify, instance, anyOfClass } from 'ts-mockito';
+import { SocialAccountCommandAdapter } from '@lib/domains/user/adapter/out/persistence/social-account.command.adapter';
+import { SocialAccountEntity } from '@lib/domains/user/domain/social-account.entity';
+import { SocialAccountSavePort } from '../../../port/out/social-account.save.port';
+import { SocialAccountCreateCommand } from '../social-account.create.command';
+import { SocialAccountCreateHandler } from '../social-account.create.handler';
+
+describe('SocialAccountCreateCommand', () => {
+  let handler: SocialAccountCreateHandler;
+  const savePort: SocialAccountSavePort = mock(SocialAccountCommandAdapter);
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      providers: [
+        SocialAccountCreateHandler,
+        {
+          provide: 'SocialAccountSavePort',
+          useValue: instance(savePort),
+        },
+      ],
+    }).compile();
+
+    handler = moduleRef.get<SocialAccountCreateHandler>(SocialAccountCreateHandler);
+  });
+
+  describe('execute', () => {
+    it('should execute create', async () => {
+      const command: SocialAccountCreateCommand = {
+        provider: 'discord',
+        socialId: 'social-id',
+        userId: 'user-id',
+        refreshToken: 'refresh-token',
+        accessToken: 'access-token',
+        expiresAt: 1234,
+        tokenType: 'token-type',
+        scope: 'scope',
+        idToken: 'id-token',
+        sessionState: 'session-state',
+      };
+      await handler.execute(command);
+      verify(savePort.create(anyOfClass(SocialAccountEntity))).once();
+    });
+  });
+});
---
+++ libs/domains/src/user/application/command/social-account-update/__tests__/social-account.update.handler.spec.ts
@@ -0,0 +1,45 @@
+import { Test } from '@nestjs/testing';
+import { mock, verify, instance, anyOfClass } from 'ts-mockito';
+import { SocialAccountCommandAdapter } from '@lib/domains/user/adapter/out/persistence/social-account.command.adapter';
+import { SocialAccountEntity } from '@lib/domains/user/domain/social-account.entity';
+import { SocialAccountSavePort } from '../../../port/out/social-account.save.port';
+import { SocialAccountUpdateCommand } from '../social-account.update.command';
+import { SocialAccountUpdateHandler } from '../social-account.update.handler';
+
+describe('SocialAccountUpdateCommand', () => {
+  let handler: SocialAccountUpdateHandler;
+  const savePort: SocialAccountSavePort = mock(SocialAccountCommandAdapter);
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      providers: [
+        SocialAccountUpdateHandler,
+        {
+          provide: 'SocialAccountSavePort',
+          useValue: instance(savePort),
+        },
+      ],
+    }).compile();
+
+    handler = moduleRef.get<SocialAccountUpdateHandler>(SocialAccountUpdateHandler);
+  });
+
+  describe('execute', () => {
+    it('should execute update', async () => {
+      const command: SocialAccountUpdateCommand = {
+        provider: 'discord',
+        socialId: 'social-id',
+        userId: 'user-id',
+        refreshToken: 'refresh-token',
+        accessToken: 'access-token',
+        expiresAt: 1234,
+        tokenType: 'token-type',
+        scope: 'scope',
+        idToken: 'id-token',
+        sessionState: 'session-state',
+      };
+      await handler.execute(command);
+      verify(savePort.update(anyOfClass(SocialAccountEntity))).once();
+    });
+  });
+});
