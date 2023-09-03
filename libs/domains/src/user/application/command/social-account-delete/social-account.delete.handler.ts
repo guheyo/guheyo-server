0a8f736ef4467fc28101f72ef981352b59aac96a
keypport <113260136+keypport@users.noreply.github.com>
2023-09-04 05:49:12 +0900

    feat: add social-account command and handler  (#32)
    * fix: change return type to Promise<void>
    
    * feat: return with new UserEntity
    
    * feat: add social account command adapter
    
    * feat: move command files to each command folder
    
    * feat: add social-account create handler
    
    * feat: add social-account update handler
    
    * feat: add social-account delete handler

---
+++ libs/domains/src/user/application/command/social-account-delete/social-account.delete.handler.ts
@@ -0,0 +1,15 @@
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { Inject } from '@nestjs/common';
+import { SocialAccountDeleteCommand } from './social-account.delete.command';
+import { SocialAccountSavePort } from '../../port/out/social-account.save.port';
+
+@CommandHandler(SocialAccountDeleteCommand)
+export class SocialAccountDeleteHandler implements ICommandHandler<SocialAccountDeleteCommand> {
+  constructor(@Inject('SocialAccountSavePort')
+    private socialAccountSavePort: SocialAccountSavePort,
+  ) {}
+
+  async execute(command: SocialAccountDeleteCommand): Promise<void> {
+    await this.socialAccountSavePort.delete(command.userId, command.provider, command.socialId);
+  }
+}
