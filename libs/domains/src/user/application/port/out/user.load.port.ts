0180345c67ae552414c7d4bb176a3650a2310275
keypport <113260136+keypport@users.noreply.github.com>
2023-09-04 19:36:39 +0900

    feat: change delete parameters to command, getBySocialAccount parameters to query  (#33)
    * fix: call repository method
    
    * feat: change delete parameters to command
    
    * feat: change parameters of getBySocialAccount to query
    
    * style: lint

--- libs/domains/src/user/application/port/out/user.load.port.ts
+++ libs/domains/src/user/application/port/out/user.load.port.ts
@@ -1,6 +1,7 @@
 import { UserEntity } from '@lib/domains/user/domain/user.entity';
+import { UserGetBySocialAccountQuery } from '../../query/user-get-by-social-account/user.get-by-social-account.query';
 
 export interface UserLoadPort {
   getById(id: string): Promise<UserEntity | null>;
-  getBySocailAccount(provider: string, socialId: string): Promise<UserEntity | null>;
+  getBySocailAccount(query: UserGetBySocialAccountQuery): Promise<UserEntity | null>;
 }
