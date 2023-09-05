d685fdd4b9a02e7fe3def6c5cdfad3471a319670
keypport <113260136+keypport@users.noreply.github.com>
2023-09-05 20:39:44 +0900

    feat: implement port in user repository (#34)
    * feat: implement save port to user.command.repository
    
    * feat: implement load port to user.query.repository

--- libs/domains/src/user/application/command/user-update/__tests__/user.update.handler.spec.ts
+++ libs/domains/src/user/application/command/user-update/__tests__/user.update.handler.spec.ts
@@ -1,6 +1,6 @@
 import { Test } from '@nestjs/testing';
 import { mock, verify, instance, anyOfClass } from 'ts-mockito';
-import { UserCommandAdapter } from '@lib/domains/user/adapter/out/persistence/user.command.adapter';
+import { UserCommandRepository } from '@lib/domains/user/adapter/out/persistence/user.command.repository';
 import { UserEntity } from '@lib/domains/user/domain/user.entity';
 import { UserSavePort } from '../../../port/out/user.save.port';
 import { UserUpdateCommand } from '../user.update.command';
@@ -8,7 +8,7 @@ import { UserUpdateHandler } from '../user.update.handler';
 
 describe('UserUpdateCommand', () => {
   let handler: UserUpdateHandler;
-  const savePort: UserSavePort = mock(UserCommandAdapter);
+  const savePort: UserSavePort = mock(UserCommandRepository);
 
   beforeEach(async () => {
     const moduleRef = await Test.createTestingModule({
--- libs/domains/src/user/application/port/out/social-account.save.port.ts
+++ libs/domains/src/user/application/port/out/social-account.save.port.ts
@@ -2,7 +2,7 @@ import { SocialAccountEntity } from '@lib/domains/user/domain/social-account.ent
 import { SocialAccountDeleteCommand } from '../../command/social-account-delete/social-account.delete.command';
 
 export interface SocialAccountSavePort {
-  create(socialAccount: SocialAccountEntity): Promise<void>;
-  update(socialAccount: SocialAccountEntity): Promise<void>;
-  delete(command: SocialAccountDeleteCommand): Promise<void>;
+  createSocialAccount(socialAccount: SocialAccountEntity): Promise<void>;
+  updateSocialAccount(socialAccount: SocialAccountEntity): Promise<void>;
+  deleteSocialAccount(command: SocialAccountDeleteCommand): Promise<void>;
 }
