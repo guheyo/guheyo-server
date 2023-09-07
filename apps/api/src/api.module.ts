bd3cbec8f404bc8264b97180254a0942a4761ffa
keypport <keypport@gmail.com>
2023-09-08 00:29:23 +0900

    feat: add ApiModule

--- apps/api/src/api.module.ts
+++ apps/api/src/api.module.ts
@@ -1,9 +1,20 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
 import { ConfigYamlModule } from '@app/api/config/config.module';
+import { UserModule } from './app/user/user.module';
 
 @Module({
-  imports: [ConfigYamlModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigYamlModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: true,
+      autoSchemaFile: join(process.cwd(), 'apps/api/src/schema.gql'),
+      sortSchema: true,
+    }),
+    UserModule,
+  ],
 })
 export class ApiModule {}
