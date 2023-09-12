83b118d00ead1b178a9438459a52b699be85f1fd
keypport <113260136+keypport@users.noreply.github.com>
2023-09-12 16:15:04 +0900

    feat: add findMyUser query handler, and user.query.module (#39)
    * feat: divide member, role response from entity
    
    * feat: add social-account-without-auth, and divide social-account response from entity
    
    * feat: add author, login-user, user responses
    
    * feat: add find-login-user-by-id
    
    * feat: add find-login-user-by-social-account
    
    * feat: add user.query.module
    
    * feat: add user query module to apps/api
    
    * feat: rename login-user to my-user
    
    * style: lint

--- libs/domains/src/member/domain/member.entity.ts
+++ libs/domains/src/member/domain/member.entity.ts
@@ -1,37 +1,22 @@
-import { Field, ID, GraphQLISODateTime } from '@nestjs/graphql';
 import { Member } from '@prisma/client';
-import { IsOptional } from 'class-validator';
-import { Exclude, Type } from 'class-transformer';
 import { RoleEntity } from './role.entity';
 
 export class MemberEntity implements Member {
+  id: string;
+
+  createdAt: Date;
+
+  updatedAt: Date;
+
+  deletedAt: Date | null;
+
+  userId: string;
+
+  guildId: string;
+
+  roles: RoleEntity[];
+
   constructor(partial: Partial<MemberEntity>) {
     Object.assign(this, partial);
   }
-
-  @Field(() => ID)
-  id: string;
-
-  @Field(() => GraphQLISODateTime)
-  createdAt: Date;
-
-  @Exclude()
-  @Field(() => GraphQLISODateTime)
-  updatedAt: Date;
-
-  @IsOptional()
-  @Exclude()
-  @Field(() => GraphQLISODateTime, { nullable: true })
-  deletedAt: Date | null;
-
-  @Field()
-  userId: string;
-
-  @Field()
-  guildId: string;
-
-  @IsOptional()
-  @Type(() => RoleEntity)
-  @Field(() => [RoleEntity], { nullable: true })
-  roles: RoleEntity[];
 }
--- libs/domains/src/user/domain/user.entity.ts
+++ libs/domains/src/user/domain/user.entity.ts
@@ -1,44 +1,23 @@
-import { Field, GraphQLISODateTime, ID } from '@nestjs/graphql';
 import { User } from '@prisma/client';
-import { Exclude, Type } from 'class-transformer';
-import { MemberEntity } from '@lib/domains/member/domain/member.entity';
-import { SocialAccountEntity } from '@lib/domains/social-account/domain/social-account.entity';
 
 export class UserEntity implements User {
+  id: string;
+
+  createdAt: Date;
+
+  updatedAt: Date;
+
+  deletedAt: Date | null;
+
+  name: string | null;
+
+  username: string;
+
+  avatarURL: string | null;
+
+  bot: boolean;
+
   constructor(partial: Partial<UserEntity>) {
     Object.assign(this, partial);
   }
-
-  @Field(() => ID)
-  id: string;
-
-  @Field(() => GraphQLISODateTime)
-  createdAt: Date;
-
-  @Field(() => GraphQLISODateTime)
-  updatedAt: Date;
-
-  @Exclude()
-  @Field(() => GraphQLISODateTime, { nullable: true })
-  deletedAt: Date | null;
-
-  @Field(() => String, { nullable: true })
-  name: string | null;
-
-  @Field()
-  username: string;
-
-  @Type(() => SocialAccountEntity)
-  @Field(() => [SocialAccountEntity], { nullable: true })
-  socialAccounts: SocialAccountEntity[];
-
-  @Field(() => String, { nullable: true })
-  avatarURL: string | null;
-
-  @Field()
-  bot: boolean;
-
-  @Type(() => MemberEntity)
-  @Field(() => [MemberEntity], { nullable: true })
-  members: MemberEntity[];
 }
