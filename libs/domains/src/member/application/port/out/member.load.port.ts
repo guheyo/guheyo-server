586fa9113f544b9cae44d4e505d611fb0d5d3955
keypport <keypport@gmail.com>
2023-09-17 02:46:13 +0900

    feat: add a MemberQueryRepository that implements MemberLoadPort

---
+++ libs/domains/src/member/application/port/out/member.load.port.ts
@@ -0,0 +1,8 @@
+import { FindMemberByUserAndGuildQuery } from '../../commands/queries/find-member-by-user-and-guild/find-member-by-user-and-guild.query';
+import { MemberWithRolesResponse } from '../../dtos/member-with-roles.response';
+
+export interface MemberLoadPort {
+  findMemberByUserAndGuild(
+    query: FindMemberByUserAndGuildQuery,
+  ): Promise<MemberWithRolesResponse | null>;
+}
