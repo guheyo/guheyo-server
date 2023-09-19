a69e5903305007b67f6ee548a756fc1127006635
keypport <keypport@gmail.com>
2023-09-19 18:59:29 +0900

    refactor: imports UserDomainModule

--- apps/api/src/app/user/user.module.ts
+++ apps/api/src/app/user/user.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
-import { UserCommandModule } from '@lib/domains/user/application/commands/user.command.module';
-import { UserQueryModule } from '@lib/domains/user/application/queries/user.query.module';
+import { UserModule as UserDomainModule } from '@lib/domains/user/user.module';
 import { UserResolver } from './user.resolver';
 
 @Module({
-  imports: [CqrsModule, UserCommandModule, UserQueryModule],
+  imports: [CqrsModule, UserDomainModule],
   providers: [UserResolver],
 })
 export class UserModule {}
