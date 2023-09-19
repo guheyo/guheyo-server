531f5bd20a31ecc9cf238ff975b0de0412426e58
keypport <keypport@gmail.com>
2023-09-20 07:24:47 +0900

    feat: join handler

---
+++ apps/bot/src/events/member/join.handler.ts
@@ -0,0 +1,33 @@
+import { UserjoinedEvent } from '@lib/domains/user/application/events/user-joined/user-joined.event';
+import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { EventBus } from '@nestjs/cqrs';
+import { Context, ContextOf, On } from 'necord';
+import { v4 as uuid4 } from 'uuid';
+
+@Injectable()
+export class JoinHandler {
+  private readonly logger = new Logger(JoinHandler.name);
+
+  constructor(
+    private readonly eventBus: EventBus,
+    private readonly configService: ConfigService,
+  ) {}
+
+  @On('guildMemberAdd')
+  public async onceReady(@Context() [member]: ContextOf<'guildMemberAdd'>) {
+    this.eventBus.publish(
+      new UserjoinedEvent({
+        userId: uuid4(),
+        username: member.user.username,
+        socialAccountId: uuid4(),
+        provider: 'discord',
+        socialId: member.user.id,
+        guildId: this.configService.get('discord.guild.id')!,
+        memberId: uuid4(),
+        roleIds: [],
+      }),
+    );
+    this.logger.log(`${member.user.username}<@${member.user.id}> joined discord server`);
+  }
+}
