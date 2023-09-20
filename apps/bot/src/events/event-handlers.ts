f46cadb8ef65c2c7f3cf5829295687cb3232d0c3
keypport <113260136+keypport@users.noreply.github.com>
2023-09-20 17:37:35 +0900

    feat: add join eventHandler in bot, add joinedUserCreated in saga (#48)
    * feat: add joined-user-created event, handler
    
    * feat: publish createdJoinedUser
    
    * feat: import providers
    
    * feat: add server.id, guild.id in config
    
    * feat: join handler
    
    * feat: add guild-member.event.guard

--- apps/bot/src/events/event-handlers.ts
+++ apps/bot/src/events/event-handlers.ts
@@ -1,4 +1,5 @@
+import { JoinHandler } from './member/join.handler';
 import { ReadyHandler } from './system/ready.handler';
 import { WarnHandler } from './system/warn.handler';
 
-export const EVENT_HANDLERS = [ReadyHandler, WarnHandler];
+export const EVENT_HANDLERS = [ReadyHandler, WarnHandler, JoinHandler];
