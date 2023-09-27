5dac8df3f0ffbb589de992ce2f2eb6cb5b12dcb5
keypport <keypport@gmail.com>
2023-09-28 06:12:05 +0900

    refactor: organize the command handleres in each folder

--- apps/bot/src/commands/command-handlers.ts
+++ apps/bot/src/commands/command-handlers.ts
@@ -1,6 +1,6 @@
-import { PingSlashCommandHandler } from './info/ping.slash-command-handler';
-import { SendMessageSlashCommandHandler } from './communication/send-message.slash-command-handler';
-import { RegisterUserSlashCommandHandler } from './operation/register-user.slash-command-handler';
+import { PingSlashCommandHandler } from './ping/ping.slash-command-handler';
+import { SendMessageSlashCommandHandler } from './send-message/send-message.slash-command-handler';
+import { RegisterUserSlashCommandHandler } from './register-user/register-user.slash-command-handler';
 
 export const COMMAND_HANDLERS = [
   PingSlashCommandHandler,
