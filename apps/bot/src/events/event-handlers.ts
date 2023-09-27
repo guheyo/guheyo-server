9fed1fb1b60bb0ff9b7f6e7f0b33e1d7436cde01
keypport <keypport@gmail.com>
2023-09-28 06:16:21 +0900

    refactor: organize event handlers in folder by context

--- apps/bot/src/events/event-handlers.ts
+++ apps/bot/src/events/event-handlers.ts
@@ -1,7 +1,7 @@
-import { JoinedMemberHandler } from './member/joined-member.handler';
-import { UpdatedMemberRolesHandler } from './member/updated-member-roles.handler';
-import { ReadyHandler } from './system/ready.handler';
-import { WarnHandler } from './system/warn.handler';
+import { JoinedMemberHandler } from './added-member/joined-member.handler';
+import { UpdatedMemberRolesHandler } from './updated-member/updated-member-roles.handler';
+import { ReadyHandler } from './ready/ready.handler';
+import { WarnHandler } from './warn/warn.handler';
 
 export const EVENT_HANDLERS = [
   ReadyHandler,
