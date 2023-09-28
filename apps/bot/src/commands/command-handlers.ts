3728ac48c385b708187d12a6b90568ff256fea4e
keypport <keypport@gmail.com>
2023-09-28 20:56:19 +0900

    feat: add UpsertRolesSlashCommandHandler

--- apps/bot/src/commands/command-handlers.ts
+++ apps/bot/src/commands/command-handlers.ts
@@ -1,9 +1,11 @@
 import { PingSlashCommandHandler } from './ping/ping.slash-command-handler';
 import { SendMessageSlashCommandHandler } from './send-message/send-message.slash-command-handler';
 import { RegisterDiscordUserSlashCommandHandler } from './register-discord-user/register-discord-user.slash-command-handler';
+import { UpsertRolesSlashCommandHandler } from './upsert-roles/upsert-roles.slash-command-handler';
 
 export const COMMAND_HANDLERS = [
   PingSlashCommandHandler,
   SendMessageSlashCommandHandler,
   RegisterDiscordUserSlashCommandHandler,
+  UpsertRolesSlashCommandHandler,
 ];
