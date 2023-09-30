a2795145be2c9bf82b9c1a4cb78c476d022d60da
keypport <keypport@gmail.com>
2023-10-01 03:49:07 +0900

    feat: remove beta src files

--- src/routes/api/guilds/categories/index.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import { getCategories } from './categories.ctrl';
-
-const router = Router({ mergeParams: true });
-router.get('/', getCategories);
-
-export default router;
