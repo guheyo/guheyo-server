9ee065dd6fbf54b0c5eef28709998f3ede5e8ecb
keypport <keypport@gmail.com>
2023-10-02 05:44:39 +0900

    feat: remove member events, sagas

--- libs/domains/src/member/application/sagas/member.saga.providers.ts
+++ libs/domains/src/member/application/sagas/member.saga.providers.ts
@@ -1,3 +1,1 @@
-import { UpdateMemberRolesSaga } from './update-member-roles/update-member-roles.saga';
-
-export const MEMBER_SAGA_PROVIDERS = [UpdateMemberRolesSaga];
+export const MEMBER_SAGA_PROVIDERS = [];
