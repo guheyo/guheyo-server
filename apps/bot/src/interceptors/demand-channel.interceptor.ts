28dead546a6ddf609a94031c9dda0ab88c0a3dfb
keypport <keypport@gmail.com>
2023-10-02 04:11:30 +0900

    feat: add offer, demand, swap channel interceptors

---
+++ apps/bot/src/interceptors/demand-channel.interceptor.ts
@@ -0,0 +1,23 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Message } from 'discord.js';
+import { values } from 'lodash';
+import { Observable, of } from 'rxjs';
+
+@Injectable()
+export class DemandChannelInterceptor implements NestInterceptor {
+  constructor(private readonly configService: ConfigService) {}
+
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    const [message]: [Message] = context.getArgByIndex(0);
+    const allowedChannels: Record<string, string> = this.configService.get(
+      'discord.server.market.wtb.channels',
+    )!;
+    const allowedChannelIds = values(allowedChannels);
+    if (!allowedChannelIds.includes(message.channel?.id ?? '')) return of([]);
+    return next.handle();
+  }
+}
