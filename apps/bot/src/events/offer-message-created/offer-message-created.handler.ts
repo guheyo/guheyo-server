c5a8059ca51bfa1a5ce3560b55630222009897f5
keypport <keypport@gmail.com>
2023-10-05 02:30:15 +0900

    feat: use pipes in OfferMessageCreatedHandler

--- apps/bot/src/events/offer-message-created/offer-message-created.handler.ts
+++ apps/bot/src/events/offer-message-created/offer-message-created.handler.ts
@@ -1,9 +1,12 @@
 import { Injectable, Logger, UseGuards } from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
-import { Context, ContextOf, On } from 'necord';
+import { Context, On } from 'necord';
 import { GuildGuard } from '@app/bot/guards/guilds/guild.guard';
 import { MarketChannelGuard } from '@app/bot/guards/channels/market-channel.guard';
 import { Type } from '@app/bot/decorators/type.decorator';
+import { UserWithMessagePipe } from '@app/bot/pipes/user/user-with-message.pipe';
+import { OfferPipe } from '@app/bot/pipes/deal/offer/offer.pipe';
+import { OfferWithUserImagesCreateInput } from '@app/bot/pipes/deal/offer/offer.types';
 
 @UseGuards(GuildGuard, MarketChannelGuard)
 @Type('wts')
@@ -14,7 +17,10 @@ export class OfferMessageCreatedHandler {
   constructor(private readonly commandBus: CommandBus) {}
 
   @On('messageCreate')
-  public async onCreateOfferMessage(@Context() [message]: ContextOf<'messageCreate'>) {
+  public async onCreateOfferMessage(
+    @Context(UserWithMessagePipe, OfferPipe)
+    { createOfferInput, createUserImagesInput }: OfferWithUserImagesCreateInput,
+  ) {
     // TODO: create created offer message
   }
 }
