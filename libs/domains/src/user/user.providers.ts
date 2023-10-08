a3b878afb05d10999294e8077f8ee0e89ea0f3b1
keypport <keypport@gmail.com>
2023-10-09 03:53:33 +0900

    feat: remove user.service

--- libs/domains/src/user/user.providers.ts
+++ libs/domains/src/user/user.providers.ts
@@ -1,7 +1,6 @@
 import { USER_COMMAND_PROVIDERS } from './application/commands/user.command.providers';
 import { USER_QUERY_PROVIDERS } from './application/queries/user.query.providers';
 import { USER_EVENT_PROVIDERS } from './application/events/user.event.providers';
-import { UserService } from './application/services/user.service';
 import { UserRepository } from './adapter/out/persistence/user.repository';
 import { UserSagas } from './application/sagas/user.sagas';
 
@@ -18,5 +17,4 @@ export const USER_PROVIDERS = [
   ...USER_QUERY_PROVIDERS,
   ...USER_EVENT_PROVIDERS,
   UserSagas,
-  UserService,
 ];
