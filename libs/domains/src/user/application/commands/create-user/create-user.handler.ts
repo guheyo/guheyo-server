8d117bd72f5cd65a53908da219a9e50e19671b1b
keypport <keypport@gmail.com>
2023-10-10 08:11:03 +0900

    feat: inject EventPublisher into CreateUserHandler

--- libs/domains/src/user/application/commands/create-user/create-user.handler.ts
+++ libs/domains/src/user/application/commands/create-user/create-user.handler.ts
@@ -1,4 +1,4 @@
-import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { CommandHandler, ICommandHandler, EventPublisher } from '@nestjs/cqrs';
 import { Inject } from '@nestjs/common';
 import { UserEntity } from '@lib/domains/user/domain/user.entity';
 import { CreateUserCommand } from './create-user.command';
@@ -6,12 +6,15 @@ import { UserSavePort } from '../../ports/out/user.save.port';
 
 @CommandHandler(CreateUserCommand)
 export class CreateUserHandler implements ICommandHandler<CreateUserCommand> {
-  constructor(@Inject('UserSavePort') private userSavePort: UserSavePort) {}
+  constructor(
+    @Inject('UserSavePort') private userSavePort: UserSavePort,
+    private readonly publisher: EventPublisher,
+  ) {}
 
   async execute(command: CreateUserCommand): Promise<void> {
-    const user = new UserEntity(command);
-    await this.userSavePort.create(user);
+    const user = this.publisher.mergeObjectContext(new UserEntity(command));
     user.create([]);
+    await this.userSavePort.create(user);
     user.commit();
   }
 }
