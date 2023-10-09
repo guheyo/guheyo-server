cbf633f6e53aa18136c76c05cd5ab2521b67e2d9
keypport <keypport@gmail.com>
2023-10-10 08:07:23 +0900

    feat: add uploadAndCreateAvatar, uploadAndCreateAttachments

--- apps/bot/src/apps/user-image/user-image.client.ts
+++ apps/bot/src/apps/user-image/user-image.client.ts
@@ -2,6 +2,8 @@ import { ImageService } from '@lib/shared';
 import { Inject, Injectable } from '@nestjs/common';
 import { QueryBus, CommandBus } from '@nestjs/cqrs';
 import { CreateUserImageCommand } from '@lib/domains/user-image/application/commands/create-user-image/create-user-image.command';
+import { CreateUserImageInput } from '@lib/domains/user-image/application/commands/create-user-image/create-user-image.input';
+import { CreateManyUserImageCommand } from '@lib/domains/user-image/application/commands/create-many-user-image/create-many-user-image.command';
 import { UserImageParser } from './user-image.parser';
 
 @Injectable()
@@ -17,6 +19,31 @@ export class UserImageClient {
     readonly commandBus: CommandBus,
   ) {}
 
+  async uploadAndCreateAvatar({
+    userId,
+    discordAvatarURL,
+  }: {
+    userId: string;
+    discordAvatarURL?: string;
+  }): Promise<string | null> {
+    if (!discordAvatarURL) return null;
+
+    const imageId = this.userImageParser.generateUUID();
+    const { url, contentType, name } = await this.uploadAvatar({
+      userId,
+      imageId,
+      discordAvatarURL,
+    });
+    await this.createAvatar({
+      id: imageId,
+      name,
+      url,
+      contentType: contentType || undefined,
+      userId,
+    });
+    return url;
+  }
+
   async uploadAvatar({
     userId,
     imageId,
@@ -30,7 +57,7 @@ export class UserImageClient {
     return this.imageService.uploadFileFromURL(discordAvatarURL, path);
   }
 
-  async createAvatarImage({
+  async createAvatar({
     id,
     name,
     url,
@@ -42,7 +69,7 @@ export class UserImageClient {
     url: string;
     contentType?: string;
     userId: string;
-  }): Promise<string | null> {
+  }): Promise<string> {
     const input = this.userImageParser.parseCreateUserImageFromAvatar({
       id,
       name,
@@ -52,5 +79,38 @@ export class UserImageClient {
     });
     await this.commandBus.execute(new CreateUserImageCommand(input));
     return id;
+  }
+
+  async uploadAndCreateAttachments(
+    uploadUserImageInputList: CreateUserImageInput[],
+  ): Promise<void> {
+    const createUserImageInputList = await this.uploadAttachments(uploadUserImageInputList);
+    await this.createAttachments(createUserImageInputList);
+  }
+
+  async uploadAttachments(
+    uploadUserImageInputList: CreateUserImageInput[],
+  ): Promise<CreateUserImageInput[]> {
+    const createUserImageInputPromiseList = uploadUserImageInputList.map(
+      async (uploadUserImageInput) => {
+        const path = this.imageService.generateUploadPath(
+          uploadUserImageInput.userId,
+          'offer',
+          uploadUserImageInput.id,
+        );
+        const { url } = await this.imageService.uploadFileFromURL(uploadUserImageInput.url, path);
+        uploadUserImageInput.url = url;
+        return uploadUserImageInput;
+      },
+    );
+    return Promise.all(createUserImageInputPromiseList);
+  }
+
+  async createAttachments(createUserImageInputList: CreateUserImageInput[]): Promise<void> {
+    await this.commandBus.execute(
+      new CreateManyUserImageCommand({
+        data: createUserImageInputList,
+      }),
+    );
   }
 }
