7045f7ed90695a7db602621343f029fe3239f10b
keypport <keypport@gmail.com>
2024-01-03 17:19:04 +0900

    feat: use DateTimeResolver in api module

--- apps/api/src/api.module.ts
+++ apps/api/src/api.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ConfigYamlModule } from '@app/api/config/config.module';
+import { DateTimeResolver, DateTimeTypeDefinition } from 'graphql-scalars';
 import { UserModule } from './app/user/user.module';
 import { SocialAccountModule } from './app/social-account/social-account.module';
 import { MemberModule } from './app/member/member.module';
@@ -24,6 +25,10 @@ import { SessionModule } from './app/session/session.module';
       playground: true,
       autoSchemaFile: join(process.cwd(), 'apps/api/src/schema.gql'),
       sortSchema: true,
+      typeDefs: [DateTimeTypeDefinition],
+      resolvers: {
+        DateTime: DateTimeResolver,
+      },
     }),
     UserModule,
     SocialAccountModule,
