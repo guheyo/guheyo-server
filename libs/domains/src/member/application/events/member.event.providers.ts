bc3678506c4c842795ab8195681108b71e62bf31
keypport <keypport@gmail.com>
2024-01-15 01:49:38 +0900

    feat: add discord-member-roles added, removed handler (#66)
    * feat: add discord-member-roles-updated
    
    * refactor: divide discord-member-roles-updated to roles-added and roles-removed
    
    * feat: remove member events, sagas
    
    * style: lint
    
    * chore : add @nestjs/microservices
    
    * feat: await commandBus.execute
    
    * feat: await this.commandBus.execute in commands
    
    * chore: add @nestjs/microservices
    
    * feat: add bot-exception filter extends BaseRpcExceptionFilter
    
    * feat: provides: APP_FILTER in bot.module

--- libs/domains/src/member/application/events/member.event.providers.ts
+++ libs/domains/src/member/application/events/member.event.providers.ts
@@ -1,3 +1,1 @@
-import { MemberRolesUpdatedHandler } from './member-roles-updated/member-roles-updated.handler';
-
-export const MEMBER_EVENT_PROVIDERS = [MemberRolesUpdatedHandler];
+export const MEMBER_EVENT_PROVIDERS = [];
