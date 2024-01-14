8268ce5cf9cc58d9160583b67a7193d5dc744cfa
keypport <keypport@gmail.com>
2024-01-15 01:40:33 +0900

    fest: prisma & redis setting (#14)
    * chore: add yaml
    
    * feat: prisma & redis setting
    
    * fix: enableShutdownHooks 지원 중단
    
    * chore: downgrade prisma from 5.0.0 to 4.13.0 - migration 미적용
    
    * style: apply eslint and prettier
    
    * fix: resolve peer dependency error
    
    ---------
    
    Co-authored-by: keypport <keypport@gmail.com>

--- apps/apis/src/apis.module.ts
+++ apps/apis/src/apis.module.ts
@@ -1,11 +1,17 @@
 import { ConfigYamlModule } from '@lib/config/config-yaml.module';
 import { Module } from '@nestjs/common';
-import { BotController } from '~bot/src/controllers/bot.controller';
-import { Interfaces } from '~bot/src/interfaces';
+import { DatabaseModule } from '@lib/database/database.module';
+import * as redisStore from 'cache-manager-redis-store';
+import { UserController } from '~apis/src/controllers/user.controller';
+import { Interfaces } from '~apis/src/interfaces';
 
 @Module({
-  imports: [ConfigYamlModule],
-  controllers: [BotController],
+  imports: [
+    ConfigYamlModule,
+    DatabaseModule.prismaModule(),
+    DatabaseModule.redisModule({ ttl: 0, store: redisStore }),
+  ],
+  controllers: [UserController],
   providers: [...Interfaces],
 })
 export class ApisModule {}
