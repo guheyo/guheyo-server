82db75ad1c126cf67f1ad4670531d09ea8f39e1d
keypport <keypport@gmail.com>
2024-01-26 04:07:22 +0900

    feat: change PrismaService to ExtendedPrismaService for type

--- libs/shared/src/cqrs/repositories/prisma-repository.ts
+++ libs/shared/src/cqrs/repositories/prisma-repository.ts
@@ -1,11 +1,12 @@
-import { PrismaService } from '@lib/shared/prisma';
+import { PRISMA_SERVICE } from '@lib/shared/prisma';
+import { ExtendedPrismaService } from '@lib/shared/prisma/extensions/prisma.extension.factory';
 import { Inject, Injectable, Type } from '@nestjs/common';
 import { BaseRepository } from './base-repository';
 
 @Injectable()
 export abstract class PrismaRepository<T> extends BaseRepository<T> {
-  @Inject()
-  protected readonly prismaService: PrismaService;
+  @Inject(PRISMA_SERVICE)
+  public readonly prismaService: ExtendedPrismaService;
 
   constructor(protected readonly Entity: Type<T>) {
     super(Entity);
