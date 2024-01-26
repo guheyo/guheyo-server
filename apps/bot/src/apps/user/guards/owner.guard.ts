1b29a5aa218b569a400e710a538c655c9b1fa70a
keypport <keypport@gmail.com>
2024-01-27 01:59:37 +0900

    feat: update guards with configParser

--- apps/bot/src/apps/user/guards/owner.guard.ts
+++ apps/bot/src/apps/user/guards/owner.guard.ts
@@ -1,15 +1,15 @@
+import { ConfigParser } from '@app/bot/shared/parsers/config.parser';
 import { CanActivate, ExecutionContext, Inject, Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { GuildMember, Interaction } from 'discord.js';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class OwnerGuard implements CanActivate {
   @Inject()
-  private readonly configService: ConfigService;
+  private readonly configParser: ConfigParser;
 
   canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
     const [instanceWithUser]: [Interaction | GuildMember] = context.getArgByIndex(0);
-    return instanceWithUser.user.id === this.configService.get('discord.bot.owner-id');
+    return instanceWithUser.user.id === this.configParser.getDiscordBotOwnerId();
   }
 }
