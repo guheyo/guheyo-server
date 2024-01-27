d11287a3bc0a0a237b3e4d3580c47855e2edfac7
keypport <keypport@gmail.com>
2024-01-27 23:35:55 +0900

    feat: add parseUserFromMessagePipe

---
+++ apps/bot/src/apps/user/pipes/parse-user-from-message.pipe.ts
@@ -0,0 +1,42 @@
+import { UserClient } from '@app/bot/apps/user/clients/user.client';
+import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
+import { ContextOf } from 'necord';
+import { UserErrorMessage } from '@app/bot/apps/user/parsers/user.error-message';
+import { UserParser } from '@app/bot/apps/user/parsers/user.parser';
+import { Message } from 'discord.js';
+import { SimpleUser } from '../parsers/user.types';
+
+@Injectable()
+export class ParseUserFromMessagePipe implements PipeTransform {
+  constructor(
+    private readonly userClient: UserClient,
+    private readonly userParser: UserParser,
+  ) {}
+
+  async transform(
+    [createdOrOldmessage, newMessage]: ContextOf<'messageCreate' | 'messageUpdate'>,
+    metadata: ArgumentMetadata,
+  ): Promise<SimpleUser> {
+    let message: Message;
+    if (newMessage) message = await newMessage.fetch();
+    else message = await createdOrOldmessage.fetch();
+
+    const discordMember = message.member;
+    if (!discordMember) throw new RpcException(UserErrorMessage.DISOCRD_MEMBER_NOT_FOUND);
+
+    const user = await this.userClient.findUserBySocialAccount('discord', discordMember.id);
+    if (user)
+      return {
+        id: user.id,
+        username: user.username,
+      };
+
+    const input = this.userParser.parseCreateUserFromDiscordInput(discordMember);
+    await this.userClient.createUserFromDiscord(input);
+    return {
+      id: input.id,
+      username: input.username,
+    };
+  }
+}
