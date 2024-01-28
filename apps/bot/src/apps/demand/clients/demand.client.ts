c681b43657585165108515275172b36d31cfd902
keypport <keypport@gmail.com>
2024-01-28 18:17:56 +0900

    refactor: move demandParser into demandClient

--- apps/bot/src/apps/demand/clients/demand.client.ts
+++ apps/bot/src/apps/demand/clients/demand.client.ts
@@ -1,13 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import { Message } from 'discord.js';
 import { CreateDemandInput } from '@lib/domains/demand/application/commands/create-demand/create-demand.input';
 import { UpdateDemandInput } from '@lib/domains/demand/application/commands/update-demand/update-demand.input';
 import { CreateDemandCommand } from '@lib/domains/demand/application/commands/create-demand/create-demand.command';
 import { UpdateDemandCommand } from '@lib/domains/demand/application/commands/update-demand/update-demand.command';
 import { DeleteDemandCommand } from '@lib/domains/demand/application/commands/delete-demand/delete-demand.command';
+import { GuildResponse } from '@lib/domains/guild/application/dtos/guild.response';
 import { UserImageClient } from '../../user-image/clients/user-image.client';
+import { DemandParser } from '../parsers/demand.parser';
 
 @Injectable()
 export class DemandClient extends UserImageClient {
+  @Inject()
+  private readonly demandParser: DemandParser;
+
+  private readonly logger = new Logger(DemandClient.name);
+
   async createDemand(input: CreateDemandInput) {
     await this.commandBus.execute(new CreateDemandCommand(input));
   }
@@ -18,5 +26,23 @@ export class DemandClient extends UserImageClient {
 
   async deleteDemand(id: string) {
     await this.commandBus.execute(new DeleteDemandCommand(id));
+  }
+
+  async createDemandFromMessage(userId: string, message: Message, guild: GuildResponse) {
+    const uploadUserImageInputList = this.userImageParser.parseUploadUserImageInputList(
+      userId,
+      message,
+      'demand',
+    );
+    const createDemandInput = this.demandParser.parseCreateDealInput(userId, message, guild);
+    await this.uploadAndCreateAttachments(uploadUserImageInputList);
+    await this.createDemand(createDemandInput);
+    this.logger.log(`Demand<@${createDemandInput.id}> created`);
+  }
+
+  async updateDemandFromMessage(message: Message) {
+    const updateDemandInput = this.demandParser.parseUpdateDealInput(message);
+    await this.updateDemand(updateDemandInput);
+    this.logger.log(`Demand<@${updateDemandInput.id}> updated`);
   }
 }
--- apps/bot/src/events/demand-message-updated/demand-message-updated.handler.ts
+++ apps/bot/src/events/demand-message-updated/demand-message-updated.handler.ts
@@ -1,21 +1,15 @@
-import { Injectable, Logger, UseGuards } from '@nestjs/common';
+import { Injectable, UseGuards } from '@nestjs/common';
 import { Context, ContextOf, On } from 'necord';
 import { GuildGuard } from '@app/bot/apps/guild/guards/guild.guard';
 import { DealChannelGuard } from '@app/bot/apps/deal/guards/deal-channel.guard';
 import { Type } from '@app/bot/decorators/type.decorator';
 import { DemandClient } from '@app/bot/apps/demand/clients/demand.client';
-import { DemandParser } from '@app/bot/apps/demand/parsers/demand.parser';
 
 @UseGuards(GuildGuard, DealChannelGuard)
 @Type('wtb')
 @Injectable()
 export class DemandMessageUpdatedHandler {
-  private readonly logger = new Logger(DemandMessageUpdatedHandler.name);
-
-  constructor(
-    private readonly demandParser: DemandParser,
-    private readonly demandClient: DemandClient,
-  ) {}
+  constructor(private readonly demandClient: DemandClient) {}
 
   @On('messageUpdate')
   public async onUpdateDemandMessage(
@@ -23,8 +17,6 @@ export class DemandMessageUpdatedHandler {
     [oldMessage, newMessage]: ContextOf<'messageUpdate'>,
   ) {
     const message = await newMessage.fetch();
-    const updateDemandInput = this.demandParser.parseUpdateDealInput(message);
-    await this.demandClient.updateDemand(updateDemandInput);
-    this.logger.log(`Demand<@${updateDemandInput.id}> updated`);
+    this.demandClient.updateDemandFromMessage(message);
   }
 }
