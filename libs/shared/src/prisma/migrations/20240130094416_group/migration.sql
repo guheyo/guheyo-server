77df44794a24a67f4b4f9ab47caa77c0c957018a
keypport <keypport@gmail.com>
2024-01-30 18:50:47 +0900

    feat: add groiup migration files

--- libs/shared/src/prisma/migrations/20240130094416_group/migration.sql
+++ libs/shared/src/prisma/migrations/20240130094416_group/migration.sql
@@ -3,7 +3,6 @@
 
   - You are about to drop the column `guildId` on the `Auction` table. All the data in the column will be lost.
   - You are about to drop the column `guildId` on the `Demand` table. All the data in the column will be lost.
-  - You are about to drop the column `discordGroupId` on the `DiscordMessage` table. All the data in the column will be lost.
   - You are about to drop the column `guildId` on the `Member` table. All the data in the column will be lost.
   - You are about to drop the column `guildId` on the `Offer` table. All the data in the column will be lost.
   - You are about to drop the column `guildId` on the `Post` table. All the data in the column will be lost.
@@ -11,9 +10,10 @@
   - You are about to drop the column `guildId` on the `ProductCategory` table. All the data in the column will be lost.
   - You are about to drop the column `guildId` on the `Role` table. All the data in the column will be lost.
   - You are about to drop the column `guildId` on the `Swap` table. All the data in the column will be lost.
+  - You are about to drop the `Guild` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `_BrandToGuild` table. If the table is not empty, all the data it contains will be lost.
   - Added the required column `groupId` to the `Auction` table without a default value. This is not possible if the table is not empty.
   - Added the required column `groupId` to the `Demand` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `discordGuildId` to the `DiscordMessage` table without a default value. This is not possible if the table is not empty.
   - Added the required column `groupId` to the `Member` table without a default value. This is not possible if the table is not empty.
   - Added the required column `groupId` to the `Offer` table without a default value. This is not possible if the table is not empty.
   - Added the required column `groupId` to the `Post` table without a default value. This is not possible if the table is not empty.
@@ -50,6 +50,12 @@ ALTER TABLE "Role" DROP CONSTRAINT "Role_guildId_fkey";
 -- DropForeignKey
 ALTER TABLE "Swap" DROP CONSTRAINT "Swap_guildId_fkey";
 
+-- DropForeignKey
+ALTER TABLE "_BrandToGuild" DROP CONSTRAINT "_BrandToGuild_A_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_BrandToGuild" DROP CONSTRAINT "_BrandToGuild_B_fkey";
+
 -- AlterTable
 ALTER TABLE "Auction" DROP COLUMN "guildId",
 ADD COLUMN     "groupId" TEXT NOT NULL;
@@ -57,10 +63,6 @@ ADD COLUMN     "groupId" TEXT NOT NULL;
 -- AlterTable
 ALTER TABLE "Demand" DROP COLUMN "guildId",
 ADD COLUMN     "groupId" TEXT NOT NULL;
-
--- AlterTable
-ALTER TABLE "DiscordMessage" DROP COLUMN "discordGroupId",
-ADD COLUMN     "discordGuildId" TEXT NOT NULL;
 
 -- AlterTable
 ALTER TABLE "Member" DROP COLUMN "guildId",
@@ -91,6 +93,42 @@ ALTER COLUMN "hexColor" SET DEFAULT '#7f838e';
 ALTER TABLE "Swap" DROP COLUMN "guildId",
 ADD COLUMN     "groupId" TEXT NOT NULL;
 
+-- DropTable
+DROP TABLE "Guild";
+
+-- DropTable
+DROP TABLE "_BrandToGuild";
+
+-- CreateTable
+CREATE TABLE "Group" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
+    "name" TEXT NOT NULL,
+    "slug" TEXT,
+    "description" TEXT,
+    "icon" TEXT,
+    "position" INTEGER NOT NULL,
+
+    CONSTRAINT "Group_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_BrandToGroup" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Group_name_key" ON "Group"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_BrandToGroup_AB_unique" ON "_BrandToGroup"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_BrandToGroup_B_index" ON "_BrandToGroup"("B");
+
 -- AddForeignKey
 ALTER TABLE "Member" ADD CONSTRAINT "Member_groupId_fkey" FOREIGN KEY ("groupId") REFERENCES "Group"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
@@ -117,3 +155,9 @@ ALTER TABLE "Swap" ADD CONSTRAINT "Swap_groupId_fkey" FOREIGN KEY ("groupId") RE
 
 -- AddForeignKey
 ALTER TABLE "Auction" ADD CONSTRAINT "Auction_groupId_fkey" FOREIGN KEY ("groupId") REFERENCES "Group"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_BrandToGroup" ADD CONSTRAINT "_BrandToGroup_A_fkey" FOREIGN KEY ("A") REFERENCES "Brand"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_BrandToGroup" ADD CONSTRAINT "_BrandToGroup_B_fkey" FOREIGN KEY ("B") REFERENCES "Group"("id") ON DELETE CASCADE ON UPDATE CASCADE;
