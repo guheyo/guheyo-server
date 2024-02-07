9ff7fa780b717bb593f85674c00046b0681ab2f8
keypport <keypport@gmail.com>
2024-02-08 04:19:03 +0900

    feat: change fastify to express, add http filter to HttpExceptionFilter

--- libs/shared/src/filters/http-exception.filter.ts
+++ libs/shared/src/filters/http-exception.filter.ts
@@ -1,15 +1,27 @@
 import { Catch, ArgumentsHost, HttpException, Logger } from '@nestjs/common';
-import { GqlArgumentsHost, GqlExceptionFilter } from '@nestjs/graphql';
-import { FastifyRequest } from 'fastify';
+import { GqlArgumentsHost, GqlContextType, GqlExceptionFilter } from '@nestjs/graphql';
+import { Request } from 'express';
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements GqlExceptionFilter {
   private readonly logger = new Logger(HttpExceptionFilter.name);
 
   catch(exception: HttpException, host: ArgumentsHost) {
-    const gqlHost = GqlArgumentsHost.create(host);
-    const { req } = gqlHost.getContext<{ req: FastifyRequest }>();
+    let req: Request;
+    if (host.getType() === 'http') {
+      const ctx = host.switchToHttp();
+      req = ctx.getRequest<Request>();
+      this.log(req, exception);
+    }
+    if (host.getType<GqlContextType>() === 'graphql') {
+      const gqlHost = GqlArgumentsHost.create(host);
+      req = gqlHost.getContext<{ req: Request }>().req;
+      this.log(req, exception);
+    }
+    return exception;
+  }
 
+  log(req: Request, exception: HttpException) {
     this.logger.error(
       {
         req: {
@@ -20,7 +32,5 @@ export class HttpExceptionFilter implements GqlExceptionFilter {
       },
       exception.stack,
     );
-
-    return exception;
   }
 }
