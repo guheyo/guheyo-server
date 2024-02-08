efda03c79fb804a639e1f4f294446130bdcfc658
keypport <keypport@gmail.com>
2024-02-08 18:25:51 +0900

    feat: move jwt strategy to jwt folder

--- apps/api/src/app/auth/jwt/jwt.strategy.ts
+++ apps/api/src/app/auth/jwt/jwt.strategy.ts
@@ -13,7 +13,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: configService.get('next-auth.secret'),
+      secretOrKey: configService.get('jwt.secret'),
     });
   }
 
