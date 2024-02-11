4f5ff9aaef141324d49a096800a00cfbfbdb215b
keypport <keypport@gmail.com>
2024-02-12 03:54:36 +0900

    feat: add AuthResolver

--- apps/api/src/app/auth/auth.module.ts
+++ apps/api/src/app/auth/auth.module.ts
@@ -4,19 +4,15 @@ import { PrismaModule } from '@lib/shared/prisma/prisma.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtService } from '@lib/shared/jwt/jwt.service';
+import { AuthResolver } from './auth.resolver';
 import { JwtAccessStrategy } from './jwt/jwt-access.strategy';
 import { JwtRefreshStrategy } from './jwt/jwt-refresh.strategy';
 import { AuthController } from './auth.controller';
 import { DiscordStrategy } from './discord/discord.strategy';
 
 @Module({
-  imports: [
-    CqrsModule,
-    PrismaModule,
-    PassportModule.register({ defaultStrategy: 'jwt' }),
-    JwtModule.register({}),
-  ],
-  providers: [JwtAccessStrategy, JwtRefreshStrategy, JwtService, DiscordStrategy],
+  imports: [CqrsModule, PrismaModule, PassportModule.register({}), JwtModule.register({})],
+  providers: [AuthResolver, JwtAccessStrategy, JwtRefreshStrategy, JwtService, DiscordStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
---
+++ apps/api/src/app/auth/auth.resolver.ts
@@ -0,0 +1,62 @@
+import { Context, Mutation, Resolver } from '@nestjs/graphql';
+import { QueryBus, CommandBus } from '@nestjs/cqrs';
+import { JwtService } from '@lib/shared/jwt/jwt.service';
+import { HttpStatus, UseGuards } from '@nestjs/common';
+import _ from 'lodash';
+import { Request, Response } from 'express';
+import { FindSocialAccountQuery } from '@lib/domains/social-account/application/queries/find-social-account/find-social-account.query';
+import { UpdateSocialAccountCommand } from '@lib/domains/social-account/application/commands/update-social-account/update-social-account.command';
+import { Payload } from '@lib/shared/jwt/jwt.interfaces';
+import { JwtRefreshAuthGuard } from './jwt/jwt-refresh-auth.guard';
+
+@Resolver()
+export class AuthResolver {
+  constructor(
+    private readonly queryBus: QueryBus,
+    private readonly commandBus: CommandBus,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  @Mutation(() => String)
+  @UseGuards(JwtRefreshAuthGuard)
+  async refreshToken(@Context('req') req: Request, @Context('res') res: Response) {
+    const oldRefreshToken = this.jwtService.getRefreshTokenFromCookies(req);
+    if (!oldRefreshToken) {
+      return res.status(HttpStatus.UNAUTHORIZED).send();
+    }
+    const payload = _.pick(this.jwtService.verifyRefreshToken(oldRefreshToken) as Payload, [
+      'username',
+      'provider',
+      'socialId',
+      'avatarURL',
+    ]);
+    const socialAccount = await this.queryBus.execute(
+      new FindSocialAccountQuery({
+        provider: payload.provider,
+        socialId: payload.socialId,
+        refreshToken: oldRefreshToken,
+      }),
+    );
+    if (!socialAccount) {
+      return res.status(HttpStatus.UNAUTHORIZED).send();
+    }
+    const accessToken = this.jwtService.signAccessToken(payload);
+    const refreshToken = this.jwtService.signRefreshToken(payload);
+    await this.commandBus.execute(
+      new UpdateSocialAccountCommand({
+        id: socialAccount.id,
+        accessToken,
+        refreshToken,
+      }),
+    );
+    this.jwtService.setAccessTokenCookie(accessToken, res);
+    this.jwtService.setRefreshTokenCookie(refreshToken, res);
+    return res.status(HttpStatus.OK).send();
+  }
+
+  @Mutation(() => String)
+  @UseGuards(JwtRefreshAuthGuard)
+  async logout(@Context('req') req: Request, @Context('res') res: Response) {
+    // TODO
+  }
+}
