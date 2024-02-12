2b08af955a4645b4bf71fe27be0bcc8aecdf0337
keypport <keypport@gmail.com>
2024-02-13 01:38:51 +0900

    feat: return string in CommonController

--- apps/api/src/app/common/common.controller.ts
+++ apps/api/src/app/common/common.controller.ts
@@ -1,15 +1,15 @@
-import { Controller, Get, HttpStatus, Res } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { Response } from 'express';
 
 @Controller()
 export class CommonController {
   @Get('favicon.ico')
-  getFavicon(@Res() res: Response) {
-    return res.status(HttpStatus.NO_CONTENT).send('ok');
+  getFavicon(@Res() res: Response): string {
+    return 'NO CONTENT';
   }
 
   @Get()
-  getNoContent(@Res() res: Response) {
-    return res.status(HttpStatus.NO_CONTENT).send('ok');
+  getNoContent(@Res() res: Response): string {
+    return 'NO CONTENT';
   }
 }
