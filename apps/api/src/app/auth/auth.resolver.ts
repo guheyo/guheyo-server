f18d92c6020c7081949ce672d289c74be03daa2c
keypport <keypport@gmail.com>
2024-02-15 19:16:23 +0900

    feat: change return type of logout in auth resolver

--- apps/api/src/app/auth/auth.resolver.ts
+++ apps/api/src/app/auth/auth.resolver.ts
@@ -3,8 +3,9 @@ import { CommandBus } from '@nestjs/cqrs';
 import { JwtService } from '@lib/shared/jwt/jwt.service';
 import { UseGuards } from '@nestjs/common';
 import { Request, Response } from 'express';
-import { UpdateSocialAccountCommand } from '@lib/domains/social-account/application/commands/update-social-account/update-social-account.command';
 import { Payload } from '@lib/shared/jwt/jwt.interfaces';
+import { UpdateSocialAccountCommand } from '@lib/domains/social-account/application/commands/update-social-account/update-social-account.command';
+import { SocialUserResponse } from '@lib/domains/social-account/application/dtos/social-user.response';
 import { JwtRefreshAuthGuard } from './jwt/jwt-refresh-auth.guard';
 import { JwtResponse } from './jwt/jwt.response';
 
@@ -40,9 +41,12 @@ export class AuthResolver {
     });
   }
 
-  @Mutation(() => String)
+  @Mutation(() => SocialUserResponse)
   @UseGuards(JwtRefreshAuthGuard)
-  async logout(@Context('req') req: Request, @Context('res') res: Response): Promise<String> {
+  async logout(
+    @Context('req') req: Request,
+    @Context('res') res: Response,
+  ): Promise<SocialUserResponse> {
     const jwtUser = this.jwtService.parseJwtUserFromPayload(req.user as Payload);
     await this.commandBus.execute(
       new UpdateSocialAccountCommand({
@@ -54,6 +58,9 @@ export class AuthResolver {
     );
     this.jwtService.clearAccessTokenCookie(res);
     this.jwtService.clearRefreshTokenCookie(res);
-    return 'logout';
+    return new SocialUserResponse({
+      provider: jwtUser.provider,
+      socialId: jwtUser.socialId,
+    });
   }
 }
