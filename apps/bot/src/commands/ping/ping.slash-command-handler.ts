70f9976e85640ea1a100aa88f943caed5b0f46c0
keypport <keypport@gmail.com>
2024-02-16 22:20:43 +0900

    feat: use OwnerGuard in all command handler

--- apps/bot/src/commands/ping/ping.slash-command-handler.ts
+++ apps/bot/src/commands/ping/ping.slash-command-handler.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { OwnerGuard } from '@app/bot/apps/user/guards/owner.guard';
+import { Injectable, UseGuards } from '@nestjs/common';
 import { Context, SlashCommand, SlashCommandContext } from 'necord';
 
+@UseGuards(OwnerGuard)
 @Injectable()
 export class PingSlashCommandHandler {
   @SlashCommand({ name: 'ping', description: 'Ping-Pong Command' })
