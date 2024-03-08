e38d775ad30f9ecabe1761dd9961aa0eb703ace1
keypport <keypport@gmail.com>
2024-03-09 05:08:58 +0900

    feat: return user field if data provided

--- libs/domains/src/auth/decorators/auth-user/auth-user.decorator.ts
+++ libs/domains/src/auth/decorators/auth-user/auth-user.decorator.ts
@@ -1,9 +1,8 @@
-import { UserPayload } from '@lib/shared/jwt/jwt.interfaces';
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
 
-export const AuthUser = createParamDecorator((data: unknown, context: ExecutionContext) => {
+export const AuthUser = createParamDecorator((data: string, context: ExecutionContext) => {
   const ctx = GqlExecutionContext.create(context);
   const { req } = ctx.getContext();
-  return req.user as UserPayload;
+  return data ? req.user?.[data] : req.user;
 });
