acb8b651eada230c4bf8713942226f457b8bb3d4
keypport <keypport@gmail.com>
2024-03-10 03:44:32 +0900

    feat: use ParseUserFrommessagePipe in Update handler in bot

--- apps/bot/src/events/offer-message-updated/offer-message-updated.handler.ts
+++ apps/bot/src/events/offer-message-updated/offer-message-updated.handler.ts
@@ -4,6 +4,8 @@ import { GroupGuard } from '@app/bot/apps/group/guards/group.guard';
 import { DealChannelGuard } from '@app/bot/apps/deal/guards/deal-channel.guard';
 import { Type } from '@app/bot/decorators/type.decorator';
 import { OfferClient } from '@app/bot/apps/offer/clients/offer.client';
+import { ParseUserFromMessagePipe } from '@app/bot/apps/user/pipes/parse-user-from-message.pipe';
+import { SimpleUser } from '@app/bot/apps/user/parsers/user.types';
 
 @UseGuards(GroupGuard, DealChannelGuard)
 @Type('wts')
@@ -13,10 +15,12 @@ export class OfferMessageUpdatedHandler {
 
   @On('messageUpdate')
   public async onUpdateOfferMessage(
+    @Context(ParseUserFromMessagePipe)
+    user: SimpleUser,
     @Context()
     [oldMessage, newMessage]: ContextOf<'messageUpdate'>,
   ) {
     const message = await newMessage.fetch();
-    await this.offerClient.updateDealFromMessage(message);
+    await this.offerClient.updateDealFromMessage(user.id, message);
   }
 }
