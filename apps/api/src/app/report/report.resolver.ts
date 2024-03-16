197b3dcc0cc1e09fd600fed60ad46c55c2a802cb
keypport <keypport@gmail.com>
2024-03-16 16:17:49 +0900

    feat: add find-reports query handler

--- apps/api/src/app/report/report.resolver.ts
+++ apps/api/src/app/report/report.resolver.ts
@@ -6,6 +6,9 @@ import { CreateReportCommand } from '@lib/domains/report/application/commands/cr
 import { ReportResponse } from '@lib/domains/report/application/dtos/report.response';
 import { FindReportArgs } from '@lib/domains/report/application/queries/find-report/find-report.args';
 import { FindReportQuery } from '@lib/domains/report/application/queries/find-report/find-report.query';
+import { PaginatedReportsResponse } from '@lib/domains/report/application/queries/find-reports/paginated-reports.response';
+import { FindReportsArgs } from '@lib/domains/report/application/queries/find-reports/find-reports.args';
+import { FindReportsQuery } from '@lib/domains/report/application/queries/find-reports/find-reports.query';
 import { GqlThrottlerBehindProxyGuard } from '../throttler/gql-throttler-behind-proxy.guard';
 
 @UseGuards(GqlThrottlerBehindProxyGuard)
@@ -25,6 +28,12 @@ export class ReportResolver {
   @Query(() => ReportResponse)
   async findReport(@Args() args: FindReportArgs) {
     const query = new FindReportQuery(args);
+    return this.queryBus.execute(query);
+  }
+
+  @Query(() => PaginatedReportsResponse)
+  async findReports(@Args() args: FindReportsArgs) {
+    const query = new FindReportsQuery(args);
     return this.queryBus.execute(query);
   }
 }
---
+++ libs/domains/src/report/application/queries/find-reports/find-reports.handler.ts
@@ -0,0 +1,51 @@
+import { QueryHandler } from '@nestjs/cqrs';
+import { PrismaQueryHandler } from '@lib/shared/cqrs/queries/handlers/prisma-query.handler';
+import { paginate } from '@lib/shared/cqrs/queries/pagination/paginate';
+import { parseFollowedBySearcher } from '@lib/shared/search/search';
+import { FindReportsQuery } from './find-reports.query';
+import { PaginatedReportsResponse } from './paginated-reports.response';
+import { ReportResponse } from '../../dtos/report.response';
+
+@QueryHandler(FindReportsQuery)
+export class FindReportsHandler extends PrismaQueryHandler<FindReportsQuery, ReportResponse> {
+  constructor() {
+    super(ReportResponse);
+  }
+
+  async execute(query: FindReportsQuery): Promise<PaginatedReportsResponse> {
+    const cursor = query.cursor
+      ? {
+          id: query.cursor,
+        }
+      : undefined;
+    const reports = await this.prismaService.report.findMany({
+      where: {
+        ...query.where,
+        title: parseFollowedBySearcher(query.keyword),
+        createdAt: query.where?.createdAt
+          ? {
+              gt: new Date(query.where.createdAt.gt),
+            }
+          : undefined,
+      },
+      cursor,
+      take: query.take + 1,
+      skip: query.skip,
+      include: {
+        comments: {
+          orderBy: {
+            createdAt: 'desc',
+          },
+        },
+      },
+      orderBy: [
+        {
+          createdAt: query.orderBy?.createdAt,
+        },
+      ],
+      distinct: query.distinct ? ['title', 'authorId'] : undefined,
+    });
+
+    return paginate<ReportResponse>(this.parseResponses(reports), 'id', query.take);
+  }
+}
