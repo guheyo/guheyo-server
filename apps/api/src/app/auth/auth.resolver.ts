7a9f30c778c294bf7b62c45b50cb5a51a46a1e68
keypport <keypport@gmail.com>
2024-03-29 04:32:57 +0900

    feat: add reGenerateTokens in auth resolver

--- apps/api/src/app/auth/auth.resolver.ts
+++ apps/api/src/app/auth/auth.resolver.ts
@@ -1,13 +1,15 @@
 import { Context, Mutation, Resolver } from '@nestjs/graphql';
-import { CommandBus } from '@nestjs/cqrs';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { JwtService } from '@lib/shared/jwt/jwt.service';
-import { UseGuards } from '@nestjs/common';
+import { NotFoundException, UseGuards } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { JwtPayload } from '@lib/shared/jwt/jwt.interfaces';
 import { UpdateSocialAccountCommand } from '@lib/domains/social-account/application/commands/update-social-account/update-social-account.command';
 import { SocialUserResponse } from '@lib/domains/social-account/application/dtos/social-user.response';
 import { JwtRefreshAuthGuard } from '@lib/domains/auth/guards/jwt/jwt-refresh-auth.guard';
 import { JwtResponse } from '@lib/domains/auth/guards/jwt/jwt.response';
+import { FindUserQuery } from '@lib/domains/user/application/queries/find-user/find-user.query';
+import { UserErrorMessage } from '@lib/domains/user/domain/user.error.message';
 import { GqlThrottlerBehindProxyGuard } from '../throttler/gql-throttler-behind-proxy.guard';
 
 @UseGuards(GqlThrottlerBehindProxyGuard)
@@ -15,6 +17,7 @@ import { GqlThrottlerBehindProxyGuard } from '../throttler/gql-throttler-behind-
 export class AuthResolver {
   constructor(
     private readonly commandBus: CommandBus,
+    private readonly queryBus: QueryBus,
     private readonly jwtService: JwtService,
   ) {}
 
@@ -26,6 +29,46 @@ export class AuthResolver {
   ): Promise<JwtResponse> {
     const jwtPayload = req.user as JwtPayload;
     const userPayload = this.jwtService.parseUserPayload(jwtPayload);
+    const accessToken = this.jwtService.signAccessToken(userPayload);
+    const refreshToken = this.jwtService.signRefreshToken(userPayload);
+    await this.commandBus.execute(
+      new UpdateSocialAccountCommand({
+        provider: jwtPayload.socialProfile.provider,
+        socialId: jwtPayload.socialProfile.id,
+        accessToken,
+        refreshToken,
+      }),
+    );
+    this.jwtService.setAccessTokenCookie(accessToken, res);
+    this.jwtService.setRefreshTokenCookie(refreshToken, res);
+    return new JwtResponse({
+      accessToken,
+      refreshToken,
+    });
+  }
+
+  @Mutation(() => JwtResponse)
+  @UseGuards(JwtRefreshAuthGuard)
+  async reGenerateTokens(
+    @Context('req') req: Request,
+    @Context('res') res: Response,
+  ): Promise<JwtResponse> {
+    const jwtPayload = req.user as JwtPayload;
+    const user = await this.queryBus.execute(
+      new FindUserQuery({
+        provider: jwtPayload.socialProfile.provider,
+        socialId: jwtPayload.socialProfile.id,
+      }),
+    );
+    if (!user) throw new NotFoundException(UserErrorMessage.USER_IS_NOT_FOUND);
+
+    const userPayload = {
+      socialProfile: jwtPayload.socialProfile,
+      id: user.id,
+      username: user.username,
+      avatarURL: user.avatarURL,
+    };
+
     const accessToken = this.jwtService.signAccessToken(userPayload);
     const refreshToken = this.jwtService.signRefreshToken(userPayload);
     await this.commandBus.execute(
