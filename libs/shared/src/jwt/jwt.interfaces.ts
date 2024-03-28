38cee97012a70604eec43dfc7b6178068c3d4a55
keypport <keypport@gmail.com>
2024-03-28 16:31:04 +0900

    feat: change Profile to SocialProfile, update UserPayload, add jwtPayload

--- libs/shared/src/jwt/jwt.interfaces.ts
+++ libs/shared/src/jwt/jwt.interfaces.ts
@@ -1,12 +1,18 @@
-export interface Profile {
+export interface SocialProfile {
   id: string;
   username: string;
   provider: string;
-  socialId: string;
   avatarURL?: string;
 }
 
-export interface UserPayload extends Profile {
+export interface UserPayload {
+  socialProfile: SocialProfile;
+  id: string;
+  username: string;
+  avatarURL?: string;
+}
+
+export interface JwtPayload extends UserPayload {
   iat: number;
   exp: number;
 }
