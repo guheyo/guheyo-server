d534f3f275828b4182774391ea50836fadd8e03a
keypport <keypport@gmail.com>
2024-03-30 21:32:24 +0900

    feat: add root blocklist role names

--- libs/domains/src/role/domain/role.types.ts
+++ libs/domains/src/role/domain/role.types.ts
@@ -5,3 +5,15 @@ export interface UpdateRoleProps {
 
   hexColor?: string;
 }
+
+export const ROOT_BLOCKLIST_ROLE_NAMES = [
+  '부계정 용의자',
+  '경매 블랙리스트',
+  '나눔 블랙리스트',
+  '비정상적인 접근',
+  '신고 자격상실',
+  '모임 블랙리스트',
+  '선제시 빌런',
+] as const;
+
+export type RootBlocklistRoleName = (typeof ROOT_BLOCKLIST_ROLE_NAMES)[number];
