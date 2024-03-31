1ea3b5f056385284b12e0a0bcbca5e8dcaa3b285
keypport <keypport@gmail.com>
2024-03-31 19:35:53 +0900

    feat: remove unused modules in api modules

--- apps/api/src/api.module.ts
+++ apps/api/src/api.module.ts
@@ -7,21 +7,15 @@ import { ConfigYamlModule } from '@app/api/config/config.module';
 import { DateTimeResolver, DateTimeTypeDefinition } from 'graphql-scalars';
 import { GraphQLJSON } from 'graphql-type-json';
 import { UserModule } from './app/user/user.module';
-import { SocialAccountModule } from './app/social-account/social-account.module';
-import { MemberModule } from './app/member/member.module';
 import { RoleModule } from './app/role/role.module';
 import { GroupModule } from './app/group/group.module';
 import { UserImageModule } from './app/user-image/user-image.module';
 import { OfferModule } from './app/offer/offer.module';
 import { DemandModule } from './app/demand/demand.module';
 import { SwapModule } from './app/swap/swap.module';
-import { AuctionModule } from './app/auction/auction.module';
-import { DiscordMessageModule } from './app/discord-message/discord-message.module';
-import { SessionModule } from './app/session/session.module';
 import { TermModule } from './app/term/term.module';
 import { AuthModule } from './app/auth/auth.module';
 import { CommonModule } from './app/common/common.module';
-import { BumpModule } from './app/bump/bump.module';
 import { ReportModule } from './app/report/report.module';
 import { CommentModule } from './app/comment/comment.module';
 import { VersionModule } from './app/version/version.module';
@@ -50,19 +44,13 @@ import { VersionModule } from './app/version/version.module';
     AuthModule,
     CommonModule,
     UserModule,
-    SocialAccountModule,
-    MemberModule,
     RoleModule,
     GroupModule,
     UserImageModule,
     OfferModule,
     DemandModule,
     SwapModule,
-    AuctionModule,
-    DiscordMessageModule,
-    SessionModule,
     TermModule,
-    BumpModule,
     ReportModule,
     CommentModule,
     VersionModule,
