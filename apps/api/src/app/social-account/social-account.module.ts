e175adaf0cb6f8ab40729761590000a04a75b5ed
keypport <keypport@gmail.com>
2024-04-02 05:05:56 +0900

    feat: not provide socialAccountResolver, memberResolver

--- apps/api/src/app/social-account/social-account.module.ts
+++ apps/api/src/app/social-account/social-account.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { PrismaModule } from '@lib/shared/prisma/prisma.module';
 import { SOCIAL_ACCOUNT_PROVIDERS } from '@lib/domains/social-account/social-account.providers';
-import { SocialAccountResolver } from './social-account.resolver';
 
 @Module({
   imports: [CqrsModule, PrismaModule],
-  providers: [SocialAccountResolver, ...SOCIAL_ACCOUNT_PROVIDERS],
+  // No SocialAccountResolver
+  providers: [...SOCIAL_ACCOUNT_PROVIDERS],
 })
 export class SocialAccountModule {}
