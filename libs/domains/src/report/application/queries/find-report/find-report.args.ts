7d343b14d537deb4d4b699e5db9e1532792704ed
keypport <keypport@gmail.com>
2024-04-02 00:58:58 +0900

    feat: update find-report args

--- libs/domains/src/report/application/queries/find-report/find-report.args.ts
+++ libs/domains/src/report/application/queries/find-report/find-report.args.ts
@@ -1,25 +1,9 @@
 import { ArgsType, Field, ID } from '@nestjs/graphql';
-import { IsOptional, IsString, IsUUID } from 'class-validator';
+import { IsUUID } from 'class-validator';
 
 @ArgsType()
 export class FindReportArgs {
-  @IsOptional()
   @IsUUID()
-  @Field(() => ID, { nullable: true })
-  id?: string;
-
-  @IsOptional()
-  @IsString()
-  @Field(() => String, { nullable: true })
-  type?: string;
-
-  @IsOptional()
-  @IsUUID()
-  @Field(() => ID, { nullable: true })
-  refId?: string;
-
-  @IsOptional()
-  @IsUUID()
-  @Field(() => ID, { nullable: true })
-  authorId?: string;
+  @Field(() => ID)
+  id: string;
 }
--- libs/domains/src/report/application/queries/find-report/find-report.query.ts
+++ libs/domains/src/report/application/queries/find-report/find-report.query.ts
@@ -2,18 +2,9 @@ import { IQuery } from '@nestjs/cqrs';
 import { FindReportArgs } from './find-report.args';
 
 export class FindReportQuery implements IQuery {
-  id?: string;
-
-  type?: string;
-
-  refId?: string;
-
-  authorId?: string;
+  id: string;
 
   constructor(args: FindReportArgs) {
     this.id = args.id;
-    this.type = args.type;
-    this.refId = args.refId;
-    this.authorId = args.authorId;
   }
 }
