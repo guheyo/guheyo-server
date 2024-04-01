65486f73e2399b49d1adab8dd9493b6831654faf
keypport <keypport@gmail.com>
2024-04-02 00:37:03 +0900

    feat: remove check-received-reports, add check-reported-user

---
+++ libs/domains/src/report/application/commands/check-reported-user/check-reported-user.handler.ts
@@ -0,0 +1,26 @@
+import { CommandHandler, EventPublisher, ICommandHandler } from '@nestjs/cqrs';
+import { Inject, NotFoundException } from '@nestjs/common';
+import { ReportErrorMessage } from '@lib/domains/report/domain/report.error.message';
+import { CheckReportedUserCommand } from './check-reported-user.command';
+import { ReportLoadPort } from '../../ports/out/report.load.port';
+import { ReportSavePort } from '../../ports/out/report.save.port';
+
+@CommandHandler(CheckReportedUserCommand)
+export class CheckReportedUserHandler implements ICommandHandler<CheckReportedUserCommand> {
+  constructor(
+    @Inject('ReportLoadPort')
+    private readonly loadPort: ReportLoadPort,
+    @Inject('ReportSavePort')
+    private readonly savePort: ReportSavePort,
+    private readonly publisher: EventPublisher,
+  ) {}
+
+  async execute(command: CheckReportedUserCommand): Promise<void> {
+    let report = await this.loadPort.findById(command.reportId);
+    if (!report) throw new NotFoundException(ReportErrorMessage.REPORT_IS_NOT_FOUND);
+
+    report = this.publisher.mergeObjectContext(report);
+    report.checkReportedUser();
+    report.commit();
+  }
+}
