1599dee434d60e701f973917175193012a5f9477
keypport <keypport@gmail.com>
2024-04-02 23:56:52 +0900

    feat: set req.user as MyUserResponse

--- libs/domains/src/auth/guards/role/root-role.guard.ts
+++ libs/domains/src/auth/guards/role/root-role.guard.ts
@@ -1,7 +1,8 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { GqlExecutionContext } from '@nestjs/graphql';
-import { JwtPayload } from '@lib/shared/jwt/jwt.interfaces';
+import { MyUserResponse } from '@lib/domains/user/application/dtos/my-user.response';
+import { ROOT_GROUP_SLUG } from '@lib/domains/group/domain/group.constants';
 import { AllowlistRoleNames } from '../../decorators/allowlist-role-names/allowlist-role-names.decorator';
 import { BlocklistRoleNames } from '../../decorators/blocklist-role-names/blocklist-role-names.decorator';
 
@@ -14,18 +15,19 @@ export class RootRoleGuard implements CanActivate {
     const { req } = ctx.getContext();
     const blocklistRoleNames = this.reflector.get(BlocklistRoleNames, context.getHandler());
     const allowlistRoleNames = this.reflector.get(AllowlistRoleNames, context.getHandler());
-    const jwtPayload = req.user as JwtPayload | null;
-    if (!jwtPayload) return false;
+    const user = req.user as MyUserResponse | null;
+    if (!user) return false;
 
+    const rootMember = user.members.find((member) => member.group.slug === ROOT_GROUP_SLUG);
     if (
       blocklistRoleNames.length > 0 &&
-      jwtPayload.rootRoleNames.some((roleName) => blocklistRoleNames.includes(roleName))
+      rootMember?.roles.some((role) => blocklistRoleNames.includes(role.name))
     )
       return false;
 
     if (
       allowlistRoleNames.length > 0 &&
-      !jwtPayload.rootRoleNames.some((roleName) => allowlistRoleNames.includes(roleName))
+      !rootMember?.roles.some((role) => allowlistRoleNames.includes(role.name))
     )
       return false;
 
