b25ba618ed7c41a547898f0baf1bbb5b02b98e4d
keypport <keypport@gmail.com>
2024-04-03 05:37:59 +0900

    feat: publish CheckReportComments from ReportCommentedEvent

--- libs/domains/src/report/application/commands/check-report-comments/check-report-comments.input.ts
+++ libs/domains/src/report/application/commands/check-report-comments/check-report-comments.input.ts
@@ -1,13 +1,9 @@
 import { Field, ID, InputType } from '@nestjs/graphql';
-import { IsString, IsUUID } from 'class-validator';
+import { IsUUID } from 'class-validator';
 
 @InputType()
 export class CheckReportCommentsInput {
-  @IsString()
-  @Field()
-  type: string;
-
   @IsUUID()
   @Field(() => ID)
-  refId: string;
+  reportId: string;
 }
--- libs/domains/src/report/application/sagas/report.sagas.ts
+++ libs/domains/src/report/application/sagas/report.sagas.ts
@@ -1,21 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { ICommand, Saga, ofType } from '@nestjs/cqrs';
 import { Observable, concatMap, filter, map, of } from 'rxjs';
-import { CreateCommentCommand } from '@lib/domains/comment/application/commands/create-comment/create-comment.command';
 import { includes, pick } from 'lodash';
-import { CommentCreatedEvent } from '@lib/domains/comment/application/events/comment-created/comment-created.event';
 import { CheckOfferReportsCommand } from '@lib/domains/offer/application/commands/check-offer-reports/check-offer-reports.command';
 import { CheckDemandReportsCommand } from '@lib/domains/demand/application/commands/check-demand-reports/check-demand-reports.command';
 import { CheckSwapReportsCommand } from '@lib/domains/swap/application/commands/check-swap-reports/check-swap-reports.command';
 import { DEAL_TYPES } from '@lib/domains/deal/domain/deal.types';
 import { ConnectRolesCommand } from '@lib/domains/member/application/commands/connect-roles/connect-roles.command';
 import { DisconnectRolesCommand } from '@lib/domains/member/application/commands/disconnect-roles/disconnect-roles.command';
-import { ReportCommentedEvent } from '../events/report-commented/report-commented.event';
 import { CheckReportCommentsCommand } from '../commands/check-report-comments/check-report-comments.command';
 import { ReportCreatedEvent } from '../events/report-created/report-created.event';
 import { ReportStatusUpdatedEvent } from '../events/report-status-updated/report-status-updated.event';
 import { CheckedReportedUserEvent } from '../events/checked-reported-user/checked-reported-user.event';
 import { CheckReportedUserCommand } from '../commands/check-reported-user/check-reported-user.command';
+import { ReportCommentedEvent } from '../events/report-commented/report-commented.event';
 
 @Injectable()
 export class ReportSagas {
@@ -23,21 +21,6 @@ export class ReportSagas {
   reportCommented = (events$: Observable<any>): Observable<ICommand> =>
     events$.pipe(
       ofType(ReportCommentedEvent),
-      map(
-        (event) =>
-          new CreateCommentCommand({
-            ...pick(event, ['id', 'authorId', 'content', 'source']),
-            type: 'report',
-            refId: event.reportId,
-          }),
-      ),
-    );
-
-  @Saga()
-  reportCommentCreated = (events$: Observable<any>): Observable<ICommand> =>
-    events$.pipe(
-      ofType(CommentCreatedEvent),
-      filter((event) => event.type === 'report'),
       map((event) => new CheckReportCommentsCommand(event)),
     );
 
