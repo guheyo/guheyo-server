14aa18ad502e873a18509b3601ac6ebce60cc2df
keypport <keypport@gmail.com>
2024-04-03 16:40:46 +0900

    feat: call countOffer from prismaService.offer in CreateOfferHandler

--- libs/domains/src/offer/application/commands/create-offer/create-offer.handler.ts
+++ libs/domains/src/offer/application/commands/create-offer/create-offer.handler.ts
@@ -1,25 +1,29 @@
-import { CommandHandler, ICommandHandler, EventPublisher } from '@nestjs/cqrs';
+import { CommandHandler, EventPublisher } from '@nestjs/cqrs';
 import { ForbiddenException, Inject } from '@nestjs/common';
 import { OfferEntity } from '@lib/domains/offer/domain/offer.entity';
 import { OfferErrorMessage } from '@lib/domains/offer/domain/offer.error.message';
 import { DAILY_OFFER_POSTING_LIMIT, DAY_HOURS } from '@lib/domains/offer/domain/offer.constants';
+import { PrismaCommandHandler } from '@lib/shared/cqrs/commands/handlers/prisma-command.handler';
 import { CreateOfferCommand } from './create-offer.command';
 import { OfferSavePort } from '../../ports/out/offer.save.port';
 import { OfferLoadPort } from '../../ports/out/offer.load.port';
+import { OfferResponse } from '../../dtos/offer.response';
 
 @CommandHandler(CreateOfferCommand)
-export class CreateOfferHandler implements ICommandHandler<CreateOfferCommand> {
+export class CreateOfferHandler extends PrismaCommandHandler<CreateOfferCommand, OfferResponse> {
   constructor(
     @Inject('OfferSavePort') private savePort: OfferSavePort,
     @Inject('OfferLoadPort') private loadPort: OfferLoadPort,
     private readonly publisher: EventPublisher,
-  ) {}
+  ) {
+    super(OfferResponse);
+  }
 
   async execute(command: CreateOfferCommand): Promise<void> {
     if (command.sellerId !== command.user.id)
       throw new ForbiddenException(OfferErrorMessage.CREATE_REQUEST_FROM_UNAUTHORIZED_USER);
 
-    const countDailyOfferPostingInSameCategory = await this.loadPort.findOfferCount({
+    const countDailyOfferPostingInSameCategory = await this.prismaService.offer.countOffer({
       sellerId: command.sellerId,
       productCategoryId: command.productCategoryId,
       fromHours: DAY_HOURS,
