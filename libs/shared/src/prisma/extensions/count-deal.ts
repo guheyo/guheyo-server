35f9aa0eeb9a7411e638da7c2d2ac4c44ff454bb
keypport <keypport@gmail.com>
2024-04-03 22:08:00 +0900

    feat: add countDemand, countSwap in extensions

--- libs/shared/src/prisma/extensions/count-deal.ts
+++ libs/shared/src/prisma/extensions/count-deal.ts
@@ -1,4 +1,6 @@
+import { FindDemandCountArgs } from '@lib/domains/demand/application/queries/find-demand-count/find-demand-count.args';
 import { FindOfferCountArgs } from '@lib/domains/offer/application/queries/find-offer-count/find-offer-count.args';
+import { FindSwapCountArgs } from '@lib/domains/swap/application/queries/find-swap-count/find-swap-count.args';
 import { Prisma, PrismaClient } from '@prisma/client';
 import dayjs from 'dayjs';
 
@@ -13,6 +15,64 @@ export const countOffer = (prisma: PrismaClient) =>
               AND: [
                 {
                   sellerId: args.sellerId,
+                },
+                {
+                  bumpedAt: {
+                    gt: dayjs().subtract(args.fromHours, 'hours').toDate(),
+                  },
+                },
+                {
+                  productCategoryId: args.productCategoryId,
+                },
+              ],
+            },
+          });
+          return count;
+        },
+      },
+    },
+  });
+
+export const countDemand = (prisma: PrismaClient) =>
+  Prisma.defineExtension({
+    name: 'countDemand',
+    model: {
+      demand: {
+        async countDemand(args: FindDemandCountArgs) {
+          const count = await prisma.demand.count({
+            where: {
+              AND: [
+                {
+                  buyerId: args.buyerId,
+                },
+                {
+                  bumpedAt: {
+                    gt: dayjs().subtract(args.fromHours, 'hours').toDate(),
+                  },
+                },
+                {
+                  productCategoryId: args.productCategoryId,
+                },
+              ],
+            },
+          });
+          return count;
+        },
+      },
+    },
+  });
+
+export const countSwap = (prisma: PrismaClient) =>
+  Prisma.defineExtension({
+    name: 'countSwap',
+    model: {
+      swap: {
+        async countSwap(args: FindSwapCountArgs) {
+          const count = await prisma.swap.count({
+            where: {
+              AND: [
+                {
+                  proposerId: args.proposerId,
                 },
                 {
                   bumpedAt: {
--- libs/shared/src/prisma/extensions/prisma.extension.factory.ts
+++ libs/shared/src/prisma/extensions/prisma.extension.factory.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client';
 import { filterSoftDeleted, hardDelete, softDelete } from './soft-delete.extension';
 import { createSlug } from './slug.extension';
 import { calculateTotalPrice } from './calculate-total-price.extension';
-import { countOffer } from './count-deal';
+import { countDemand, countOffer, countSwap } from './count-deal';
 
 export const prismaExtensionFactory = (client: PrismaClient) =>
   client
@@ -11,6 +11,8 @@ export const prismaExtensionFactory = (client: PrismaClient) =>
     .$extends(filterSoftDeleted)
     .$extends(createSlug)
     .$extends(calculateTotalPrice)
-    .$extends(countOffer(client));
+    .$extends(countOffer(client))
+    .$extends(countDemand(client))
+    .$extends(countSwap(client));
 
 export type ExtendedPrismaService = ReturnType<typeof prismaExtensionFactory>;
