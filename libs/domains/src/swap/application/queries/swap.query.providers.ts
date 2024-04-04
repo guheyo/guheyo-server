0367578ed251a599d5618d637334c7254e3d7fda
keypport <keypport@gmail.com>
2024-04-05 05:45:06 +0900

    feat: remove find-by-id query in deal domain

--- libs/domains/src/swap/application/queries/swap.query.providers.ts
+++ libs/domains/src/swap/application/queries/swap.query.providers.ts
@@ -1,10 +1,8 @@
-import { FindSwapByIdHandler } from './find-swap-by-id/find-swap-by-id.handler';
 import { FindSwapHandler } from './find-swap/find-swap.handler';
 import { FindSwapPreviewsHandler } from './find-swap-previews/find-swap-previews.handler';
 import { FindSwapCountHandler } from './find-swap-count/find-swap-count.handler';
 
 export const SWAP_QUERY_PROVIDERS = [
-  FindSwapByIdHandler,
   FindSwapHandler,
   FindSwapPreviewsHandler,
   FindSwapCountHandler,
