de2e40f96788b249abcf0a9fee7baf61fdb533ef
keypport <keypport@gmail.com>
2024-04-04 22:49:05 +0900

    feat: send webhook after demand created

--- libs/domains/src/discord-webhook/application/sagas/discord-webhook.sagas.ts
+++ libs/domains/src/discord-webhook/application/sagas/discord-webhook.sagas.ts
@@ -3,6 +3,7 @@ import { ICommand, Saga, ofType } from '@nestjs/cqrs';
 import { Observable, filter, map } from 'rxjs';
 import { OfferCreatedEvent } from '@lib/domains/offer/application/events/offer-created/offer-created.event';
 import { ConfigService } from '@nestjs/config';
+import { DemandCreatedEvent } from '@lib/domains/demand/application/events/demand-created/demand-created.event';
 import { SendDiscordWebhookCommand } from '../commands/send-discord-webhook/send-discord-webhook.command';
 
 @Injectable()
@@ -22,6 +23,24 @@ export class DiscordWebhookSagas {
             username: event.username,
             avatarURL: event.avatarURL,
             title: `[팝니다] ${event.name} - ${event.price}`,
+            url: this.parseUrl({ username: event.username, type: 'offer', slug: event.slug! }),
+          }),
+      ),
+    );
+
+  @Saga()
+  demandCreated = (events$: Observable<any>): Observable<ICommand> =>
+    events$.pipe(
+      ofType(DemandCreatedEvent),
+      filter((event) => event.source === 'mobile' || event.source === 'browser'),
+      filter((event) => !!event.slug),
+      map(
+        (event) =>
+          new SendDiscordWebhookCommand({
+            color: 0x16a34a,
+            username: event.username,
+            avatarURL: event.avatarURL,
+            title: `[삽니다] ${event.name} - ${event.price}`,
             url: this.parseUrl({ username: event.username, type: 'offer', slug: event.slug! }),
           }),
       ),
