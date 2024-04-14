c900f0a9772a03389f7084fa506183ad8917580f
keypport <keypport@gmail.com>
2024-04-14 21:36:15 +0900

    feat: add post, post-preview response

---
+++ libs/domains/src/post/application/dtos/post.response.ts
@@ -0,0 +1,22 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+import { UserImageResponse } from '@lib/domains/user-image/application/dtos/user-image.response';
+import { GroupProfileResponse } from '@lib/domains/group/application/dtos/group-profile.response';
+import { CategoryResponse } from '@lib/domains/group/application/dtos/category.response';
+import { PostPreviewResponse } from './post-preview.response';
+
+@ObjectType()
+export class PostResponse extends PostPreviewResponse {
+  @Field(() => [UserImageResponse])
+  images: UserImageResponse[];
+
+  @Field(() => GroupProfileResponse)
+  group: GroupProfileResponse;
+
+  @Field(() => CategoryResponse)
+  category: CategoryResponse;
+
+  constructor(partial: Partial<PostResponse>) {
+    super(partial);
+    Object.assign(this, partial);
+  }
+}
