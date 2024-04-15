c77cb187b5b53733b84c576528c69cbec94a80c6
keypport <keypport@gmail.com>
2024-04-16 05:44:52 +0900

    feat: update create auction handler

--- libs/domains/src/auction/application/commands/create-auction/create-auction.input.ts
+++ libs/domains/src/auction/application/commands/create-auction/create-auction.input.ts
@@ -1,9 +1,13 @@
 import { AUCTION_OPEN } from '@lib/domains/auction/domain/auction.constants';
+import { CreatePostInput } from '@lib/domains/post/application/commands/create-post/create-post.input';
 import { Field, ID, InputType } from '@nestjs/graphql';
 import { IsDate, IsOptional, IsString, IsUUID } from 'class-validator';
 
 @InputType()
 export class CreateAuctionInput {
+  @Field(() => CreatePostInput)
+  post: CreatePostInput;
+
   @IsUUID()
   @Field(() => ID)
   id: string;
@@ -13,44 +17,19 @@ export class CreateAuctionInput {
   createdAt: Date;
 
   @IsDate()
-  @Field()
-  endedAt: Date;
+  @Field(() => Date)
+  originalEndDate: Date;
 
   @IsString()
   @Field()
-  name: string;
+  shippingCost: number;
 
   @IsOptional()
   @IsString()
-  @Field(() => String, { nullable: true })
-  description?: string;
-
-  @IsString()
   @Field()
-  businessFunction: string;
+  shippingType: string;
 
   @IsString()
   @Field(() => String, { defaultValue: AUCTION_OPEN })
   status: string;
-
-  @IsString()
-  @Field()
-  source: string;
-
-  @IsUUID()
-  @Field(() => ID)
-  groupId: string;
-
-  @IsUUID()
-  @Field()
-  productCategoryId: string;
-
-  @IsUUID()
-  @Field()
-  sellerId: string;
-
-  @IsOptional()
-  @IsUUID()
-  @Field(() => String, { nullable: true })
-  brandId?: string;
 }
