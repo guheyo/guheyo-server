d98b8916801366c348f66b8a76af2935ae4b49ec
keypport <keypport@gmail.com>
2024-04-16 05:51:47 +0900

    feat: add auction-preview response

---
+++ libs/domains/src/auction/application/dtos/auction-preview.response.ts
@@ -0,0 +1,45 @@
+import { Field, ID, ObjectType, Int } from '@nestjs/graphql';
+import { PostPreviewResponse } from '@lib/domains/post/application/dtos/post-preview.response';
+
+@ObjectType()
+export class AuctionPreviewResponse {
+  @Field(() => PostPreviewResponse)
+  post: PostPreviewResponse;
+
+  @Field(() => ID)
+  id: string;
+
+  @Field(() => Date)
+  createdAt: Date;
+
+  @Field(() => Date)
+  originalEndDate: Date;
+
+  @Field(() => Date)
+  extendedEndDate: Date;
+
+  @Field(() => Date)
+  endedAt: Date;
+
+  @Field(() => Int)
+  extensionCount: number;
+
+  @Field(() => Int)
+  currentBidPrice: Number;
+
+  @Field(() => Int)
+  hammerPrice: Number;
+
+  @Field(() => Int)
+  shippingCost: Number;
+
+  @Field()
+  shippingType: string;
+
+  @Field()
+  status: string;
+
+  constructor(partial: Partial<AuctionPreviewResponse>) {
+    Object.assign(this, partial);
+  }
+}
--- libs/domains/src/auction/application/dtos/auction.response.ts
+++ libs/domains/src/auction/application/dtos/auction.response.ts
@@ -1,65 +1,19 @@
-import { Field, ID, ObjectType, Int } from '@nestjs/graphql';
-import { UserImageResponse } from '@lib/domains/user-image/application/dtos/user-image.response';
-import { AuthorResponse } from '@lib/domains/user/application/dtos/author.response';
+import { Field, ObjectType } from '@nestjs/graphql';
+import { PostResponse } from '@lib/domains/post/application/dtos/post.response';
 import { BidResponse } from './bid.response';
+import { AuctionPreviewResponse } from './auction-preview.response';
 
 @ObjectType()
-export class AuctionResponse {
-  @Field(() => ID)
-  id: string;
-
-  @Field()
-  createdAt: Date;
-
-  @Field()
-  updatedAt: Date;
-
-  @Field()
-  endedAt: Date;
-
-  @Field()
-  name: string;
-
-  @Field(() => String, { nullable: true })
-  slug: string | null;
-
-  @Field(() => String, { nullable: true })
-  description: string | null;
-
-  @Field(() => Int)
-  price: number;
-
-  @Field()
-  priceCurrency: string;
-
-  @Field()
-  businessFunction: string;
-
-  @Field()
-  status: string;
-
-  @Field()
-  source: string;
-
-  @Field(() => [UserImageResponse])
-  images: UserImageResponse[];
-
-  @Field()
-  groupId: string;
-
-  @Field()
-  productCategoryId: string;
-
-  @Field(() => AuthorResponse)
-  seller: AuthorResponse;
-
-  @Field(() => String, { nullable: true })
-  brandId: string | null;
+export class AuctionResponse extends AuctionPreviewResponse {
+  @Field(() => PostResponse)
+  post: PostResponse;
 
   @Field(() => [BidResponse])
   bids: BidResponse[];
 
   constructor(partial: Partial<AuctionResponse>) {
+    super(partial);
     Object.assign(this, partial);
+    this.post = partial.post!;
   }
 }
