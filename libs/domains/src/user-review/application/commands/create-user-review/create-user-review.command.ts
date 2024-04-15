0ea9a9c2c9a40e85d02661cb6e7fd3dc09840844
keypport <keypport@gmail.com>
2024-04-16 03:05:01 +0900

    feat: add create-user-review handler

---
+++ libs/domains/src/user-review/application/commands/create-user-review/create-user-review.command.ts
@@ -0,0 +1,33 @@
+import { ICommand } from '@nestjs/cqrs/dist';
+import { MyUserResponse } from '@lib/domains/user/application/dtos/my-user.response';
+import { CreatePostInput } from '@lib/domains/post/application/commands/create-post/create-post.input';
+import { CreateUserReviewInput } from './create-user-review.input';
+
+export class CreateUserReviewCommand implements ICommand {
+  post: CreatePostInput;
+
+  id: string;
+
+  type: string;
+
+  offerId?: string;
+
+  auctionId?: string;
+
+  reviewedUserId: string;
+
+  rating: number;
+
+  user: MyUserResponse;
+
+  constructor({ input, user }: { input: CreateUserReviewInput; user: MyUserResponse }) {
+    this.post = input.post;
+    this.id = input.id;
+    this.type = input.type;
+    this.offerId = input.offerId;
+    this.auctionId = input.auctionId;
+    this.reviewedUserId = input.reviewedUserId;
+    this.rating = input.rating;
+    this.user = user;
+  }
+}
