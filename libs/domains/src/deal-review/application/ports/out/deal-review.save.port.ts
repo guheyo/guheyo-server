eb428442e85af1ad551ad699fedc153ad053ba26
keypport <keypport@gmail.com>
2024-04-15 21:54:48 +0900

    feat: remove deal-review domain

--- libs/domains/src/deal-review/application/ports/out/deal-review.save.port.ts
+++
@@ -1,6 +0,0 @@
-import { DealReviewEntity } from '@lib/domains/deal-review/domain/deal-review.entity';
-import { SavePort } from '@lib/shared/cqrs/ports/save.port';
-
-export interface DealReviewSavePort extends SavePort<DealReviewEntity> {
-  connectMannerTags(reviewId: string, tagIds: string[]): Promise<void>;
-}
