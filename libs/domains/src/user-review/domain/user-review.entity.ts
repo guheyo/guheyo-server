219dd0b5c9ce8f60b6005246078bf0930370fc7f
keypport <keypport@gmail.com>
2024-04-16 03:03:40 +0900

    feat: update constructor of user-review entity

--- libs/domains/src/user-review/domain/user-review.entity.ts
+++ libs/domains/src/user-review/domain/user-review.entity.ts
@@ -4,6 +4,7 @@ import { Type } from 'class-transformer';
 import { PostEntity } from '@lib/domains/post/domain/post.entity';
 import { UserReviewCreatedEvent } from '../application/events/user-review-created/user-review-created.event';
 import { USER_REVIEW_ONE_WAY, USER_REVIEW_TWO_WAY } from './user-review.constants';
+import { CreateUserReviewCommand } from '../application/commands/create-user-review/create-user-review.command';
 
 export class UserReviewEntity extends AggregateRoot {
   id: string;
@@ -34,16 +35,24 @@ export class UserReviewEntity extends AggregateRoot {
 
   status: string;
 
-  constructor(partial: Partial<UserReviewEntity>) {
+  constructor(command: CreateUserReviewCommand) {
     super();
-    Object.assign(this, partial);
-    this.status = USER_REVIEW_ONE_WAY;
+    const post = new PostEntity({
+      ...command.post,
+      userId: command.user.id,
+    });
+    Object.assign(this, {
+      ...command,
+      post,
+      status: USER_REVIEW_ONE_WAY,
+    });
   }
 
   create() {
     this.apply(
       new UserReviewCreatedEvent({
         reviewId: this.id,
+        type: this.type,
         offerId: this.offerId || undefined,
         auctionId: this.auctionId || undefined,
         userId: this.post.userId,
