7eecf60c3ef11e2ed606143a237dda14d9b2612a
keypport <keypport@gmail.com>
2024-04-16 16:22:25 +0900

    feat: override user in post response

--- libs/domains/src/post/application/dtos/post.response.ts
+++ libs/domains/src/post/application/dtos/post.response.ts
@@ -2,6 +2,7 @@ import { Field, ObjectType } from '@nestjs/graphql';
 import { UserImageResponse } from '@lib/domains/user-image/application/dtos/user-image.response';
 import { GroupProfileResponse } from '@lib/domains/group/application/dtos/group-profile.response';
 import { CategoryResponse } from '@lib/domains/group/application/dtos/category.response';
+import { AuthorResponse } from '@lib/domains/user/application/dtos/author.response';
 import { PostPreviewResponse } from './post-preview.response';
 
 @ObjectType()
@@ -15,8 +16,12 @@ export class PostResponse extends PostPreviewResponse {
   @Field(() => CategoryResponse)
   category: CategoryResponse;
 
+  @Field(() => AuthorResponse)
+  user: AuthorResponse;
+
   constructor(partial: Partial<PostResponse>) {
     super(partial);
     Object.assign(this, partial);
+    this.user = partial.user!;
   }
 }
