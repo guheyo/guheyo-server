a8f14c6fde6fb9beb923763ddab030e3074b149a
keypport <113260136+keypport@users.noreply.github.com>
2024-04-20 05:45:28 +0900

    feat: exclude post version, include reportedPost in report response (#199)
    * feat: add post-preview-without-user response, add reportedPost in reportResponse
    
    * feat: exclude post version, include reportedPost in report response

--- libs/domains/src/post/application/dtos/post-preview.response.ts
+++ libs/domains/src/post/application/dtos/post-preview.response.ts
@@ -1,51 +1,14 @@
-import { Field, ID, ObjectType, Int } from '@nestjs/graphql';
+import { Field, ObjectType } from '@nestjs/graphql';
 import { UserResponse } from '@lib/domains/user/application/dtos/user.response';
+import { PostPreviewWithoutUserResponse } from './post-preview-without-user.response';
 
 @ObjectType()
-export class PostPreviewResponse {
-  @Field(() => ID)
-  id: string;
-
-  @Field()
-  createdAt: Date;
-
-  @Field()
-  updatedAt: Date;
-
-  @Field(() => Date, { nullable: true })
-  archivedAt: Date | null;
-
-  @Field(() => String, { nullable: true })
-  pending: string | null;
-
-  @Field()
-  type: string;
-
-  @Field()
-  title: string;
-
-  @Field(() => String, { nullable: true })
-  slug: string | null;
-
-  @Field(() => String, { nullable: true })
-  thumbnail: string | null;
-
-  @Field()
-  groupId: string;
-
-  @Field()
-  categoryId: string;
-
+export class PostPreviewResponse extends PostPreviewWithoutUserResponse {
   @Field(() => UserResponse)
   user: UserResponse;
 
-  @Field(() => Int)
-  reportCount: number;
-
-  @Field(() => Int)
-  reportCommentCount: number;
-
   constructor(partial: Partial<PostPreviewResponse>) {
+    super(partial);
     Object.assign(this, partial);
   }
 }
--- libs/domains/src/report/application/dtos/report.response.ts
+++ libs/domains/src/report/application/dtos/report.response.ts
@@ -1,10 +1,14 @@
 import { Field, ObjectType } from '@nestjs/graphql';
 import { VersionResponse } from '@lib/domains/version/application/dtos/version.response';
+import { PostPreviewWithoutUserResponse } from '@lib/domains/post/application/dtos/post-preview-without-user.response';
 import { ReportPreviewResponse } from './report-preview.response';
 import { ReportCommentResponse } from './report-comment.response';
 
 @ObjectType()
 export class ReportResponse extends ReportPreviewResponse {
+  @Field(() => PostPreviewWithoutUserResponse, { nullable: true })
+  reportedPost: PostPreviewWithoutUserResponse | null;
+
   @Field(() => [ReportCommentResponse])
   comments: ReportCommentResponse[];
 
