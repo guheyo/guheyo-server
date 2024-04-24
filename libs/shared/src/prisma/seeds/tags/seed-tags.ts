79e6900d19c7a888ff1520da8f60f56df25dbe43
keypport <keypport@gmail.com>
2024-04-24 22:18:14 +0900

    feat: update tag seeds

--- libs/shared/src/prisma/seeds/tags/seed-tags.ts
+++ libs/shared/src/prisma/seeds/tags/seed-tags.ts
@@ -1,43 +1,43 @@
 import { PrismaClient } from '@prisma/client';
 
-const MANNERS_TAG = 'manners';
-const NON_MANNERS_TAG = 'nonManners';
+const MANNER_TAG = 'manner';
+const BAD_MANNER_TAG = 'badManner';
 
 export async function seedTags(prisma: PrismaClient) {
   const tags = await prisma.tag.createMany({
     data: [
       {
-        type: MANNERS_TAG,
+        type: MANNER_TAG,
         name: '친절한 매너',
         position: 0,
       },
       {
-        type: MANNERS_TAG,
+        type: MANNER_TAG,
         name: '빠른 응답',
         position: 1,
       },
       {
-        type: MANNERS_TAG,
+        type: MANNER_TAG,
         name: '빠른 배송',
         position: 2,
       },
       {
-        type: MANNERS_TAG,
+        type: MANNER_TAG,
         name: '설명 일치',
         position: 3,
       },
       {
-        type: MANNERS_TAG,
+        type: MANNER_TAG,
         name: '꿀매',
         position: 4,
       },
       {
-        type: MANNERS_TAG,
+        type: MANNER_TAG,
         name: '쿨거래',
         position: 5,
       },
       {
-        type: NON_MANNERS_TAG,
+        type: BAD_MANNER_TAG,
         name: '비매너',
         position: 6,
       },
