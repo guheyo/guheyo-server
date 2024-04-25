b1982c656d3167ce63e7c0c7c8dd5d63b04535f9
keypport <keypport@gmail.com>
2024-04-25 23:11:45 +0900

    feat: check existence in seed-tags

--- libs/shared/src/prisma/seeds/tags/seed-tags.ts
+++ libs/shared/src/prisma/seeds/tags/seed-tags.ts
@@ -4,46 +4,64 @@ const MANNER_TAG = 'manner';
 const BAD_MANNER_TAG = 'badManner';
 
 export async function seedTags(prisma: PrismaClient) {
-  const tags = await prisma.tag.createMany({
-    data: [
-      {
-        type: MANNER_TAG,
-        name: '친절한 매너',
-        position: 0,
-      },
-      {
-        type: MANNER_TAG,
-        name: '빠른 응답',
-        position: 1,
-      },
-      {
-        type: MANNER_TAG,
-        name: '빠른 배송',
-        position: 2,
-      },
-      {
-        type: MANNER_TAG,
-        name: '설명 일치',
-        position: 3,
-      },
-      {
-        type: MANNER_TAG,
-        name: '꿀매',
-        position: 4,
-      },
-      {
-        type: MANNER_TAG,
-        name: '쿨거래',
-        position: 5,
-      },
-      {
-        type: BAD_MANNER_TAG,
-        name: '비매너',
-        position: 6,
-      },
-    ],
+  const mannerTags = await prisma.tag.findMany({
+    where: {
+      type: MANNER_TAG,
+    },
   });
-
+  if (!mannerTags.length) {
+    await prisma.tag.createMany({
+      data: [
+        {
+          type: MANNER_TAG,
+          name: '친절한 매너',
+          position: 0,
+        },
+        {
+          type: MANNER_TAG,
+          name: '빠른 응답',
+          position: 1,
+        },
+        {
+          type: MANNER_TAG,
+          name: '빠른 배송',
+          position: 2,
+        },
+        {
+          type: MANNER_TAG,
+          name: '설명 일치',
+          position: 3,
+        },
+        {
+          type: MANNER_TAG,
+          name: '꿀매',
+          position: 4,
+        },
+        {
+          type: MANNER_TAG,
+          name: '쿨거래',
+          position: 5,
+        },
+      ],
+    });
+  }
+  const badMannerTags = await prisma.tag.findMany({
+    where: {
+      type: BAD_MANNER_TAG,
+    },
+  });
+  if (!badMannerTags.length) {
+    await prisma.tag.createMany({
+      data: [
+        {
+          type: BAD_MANNER_TAG,
+          name: '비매너',
+          position: 6,
+        },
+      ],
+    });
+  }
   // eslint-disable-next-line no-console
-  console.log(tags);
+  console.log(mannerTags);
+  console.log(badMannerTags);
 }
