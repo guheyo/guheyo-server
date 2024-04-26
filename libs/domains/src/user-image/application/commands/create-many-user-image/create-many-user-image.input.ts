e6e1134d5b234c3da790efaef522ce3879edcbe9
keypport <keypport@gmail.com>
2024-04-26 23:15:34 +0900

    feat: set @ValidateNested() in CreateManyUserImageInput.data

--- libs/domains/src/user-image/application/commands/create-many-user-image/create-many-user-image.input.ts
+++ libs/domains/src/user-image/application/commands/create-many-user-image/create-many-user-image.input.ts
@@ -1,10 +1,12 @@
-import { IsArray } from 'class-validator';
+import { ValidateNested } from 'class-validator';
 import { Field, InputType } from '@nestjs/graphql';
+import { Type } from 'class-transformer';
 import { CreateUserImageInput } from '../create-user-image/create-user-image.input';
 
 @InputType()
 export class CreateManyUserImageInput {
-  @IsArray()
+  @ValidateNested()
+  @Type(() => CreateUserImageInput)
   @Field(() => [CreateUserImageInput])
   data: CreateUserImageInput[];
 }
