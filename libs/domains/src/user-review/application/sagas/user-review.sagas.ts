e80246fabd94474fd27f32f313f4293f5515ec08
keypport <keypport@gmail.com>
2024-04-28 03:25:08 +0900

    feat: send TrackUserImagesCommand, UpdateThumbnailCommand in UserReviewSagas.created

--- libs/domains/src/user-review/application/sagas/user-review.sagas.ts
+++ libs/domains/src/user-review/application/sagas/user-review.sagas.ts
@@ -2,8 +2,11 @@ import { Injectable } from '@nestjs/common';
 import { ICommand, Saga, ofType } from '@nestjs/cqrs';
 import { Observable, concatMap, of } from 'rxjs';
 import { ConnectTagsCommand } from '@lib/domains/post/application/commands/connect-tags/connect-tags.command';
+import { TrackUserImagesCommand } from '@lib/domains/user-image/application/commands/track-user-images/track-user-images.command';
+import { UpdateThumbnailCommand } from '@lib/domains/post/application/commands/update-thumbnail/update-thumbnail.command';
 import { UserReviewCreatedEvent } from '../events/user-review-created/user-review-created.event';
 import { CheckOtherUserReviewCommand } from '../commands/check-other-user-review/check-other-user-review.command';
+import { USER_REVIEW } from '../../domain/user-review.constants';
 
 @Injectable()
 export class UserReviewSagas {
@@ -13,6 +16,15 @@ export class UserReviewSagas {
       ofType(UserReviewCreatedEvent),
       concatMap((event) =>
         of(
+          new TrackUserImagesCommand({
+            type: USER_REVIEW,
+            refId: event.reviewId,
+          }),
+          new UpdateThumbnailCommand({
+            postId: event.postId,
+            type: USER_REVIEW,
+            refId: event.reviewId,
+          }),
           new CheckOtherUserReviewCommand({
             sourceUserReviewId: event.reviewId,
             reviewStatus: event.reviewStatus,
