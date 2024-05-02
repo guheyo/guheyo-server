418ec3abd5f1fae87c18db7a626a5a45f76ffd25
keypport <keypport@gmail.com>
2024-05-03 04:04:45 +0900

    feat: remove member domain

--- libs/domains/src/member/application/events/member.event.providers.ts
+++
@@ -1,1 +0,0 @@
-export const MEMBER_EVENT_PROVIDERS = [];
--- libs/domains/src/member/domain/member.error.message.ts
+++
@@ -1,5 +0,0 @@
-export enum MemberErrorMessage {
-  MEMBER_IS_NOT_FOUND = 'Member is not found',
-  INVALID_GROUP_PARAMETERS = 'Invalid group parameters',
-  GROUP_NOT_FOUND = 'Group not found',
-}
