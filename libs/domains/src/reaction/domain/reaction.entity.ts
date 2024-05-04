f75445ba7c97063267a08caea3028d7d21645779
keypport <keypport@gmail.com>
2024-05-04 21:12:44 +0900

    feat: add reaction entity, errorMessage

---
+++ libs/domains/src/reaction/domain/reaction.entity.ts
@@ -0,0 +1,34 @@
+import { AggregateRoot } from '@nestjs/cqrs';
+
+export class ReactionEntity extends AggregateRoot {
+  id: string;
+
+  createdAt: Date;
+
+  updateedAt: Date;
+
+  deletedAt: Date | null;
+
+  canceledAt: Date | null;
+
+  emojiId: string;
+
+  userId: string;
+
+  postId: string | null;
+
+  commentId: string | null;
+
+  constructor(partial: Partial<ReactionEntity>) {
+    super();
+    Object.assign(this, partial);
+  }
+
+  isAuthorized(userId: string) {
+    return this.userId === userId;
+  }
+
+  cancel() {
+    this.canceledAt = null;
+  }
+}
---
+++ libs/domains/src/reaction/domain/reaction.error.message.ts
@@ -0,0 +1,4 @@
+export enum ReactionErrorMessage {
+  REACTION_NOT_FOUND = 'Reaction not found',
+  REACTION_CANCEL_REQUEST_FROM_UNAUTHORIZED_USER = 'Reaction cancel request from unauthorized user',
+}
