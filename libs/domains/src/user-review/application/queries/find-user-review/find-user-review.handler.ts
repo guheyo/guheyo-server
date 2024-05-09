a2ec9da241ced38e4d316585ed8e3381d402ee9e
keypport <keypport@gmail.com>
2024-05-10 03:02:18 +0900

    feat: filter deleted comments in comment count

--- libs/domains/src/user-review/application/queries/find-user-review/find-user-review.handler.ts
+++ libs/domains/src/user-review/application/queries/find-user-review/find-user-review.handler.ts
@@ -45,6 +45,9 @@ export class FindUserReviewHandler extends PrismaQueryHandler<
             },
             tags: true,
             comments: {
+              where: {
+                deletedAt: null,
+              },
               select: {
                 id: true,
               },
