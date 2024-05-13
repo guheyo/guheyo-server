a6e409193ec6bbfd843d02bd40bb6b55d7140756
keypport <keypport@gmail.com>
2024-05-13 17:56:49 +0900

    feat: add userReviewCreated in DiscordWebhook sagas

--- libs/domains/src/discord-webhook/application/sagas/discord-webhook.sagas.ts
+++ libs/domains/src/discord-webhook/application/sagas/discord-webhook.sagas.ts
@@ -7,6 +7,7 @@ import { OFFER_BUSINESS_FUNCTIONS } from '@lib/domains/offer/domain/offer.types'
 import { includes } from 'lodash';
 import { OFFER } from '@lib/domains/offer/domain/offer.constants';
 import { ReportCreatedEvent } from '@lib/domains/report/application/events/report-created/report-created.event';
+import { UserReviewCreatedEvent } from '@lib/domains/user-review/application/events/user-review-created/user-review-created.event';
 import { SendDiscordWebhookCommand } from '../commands/send-discord-webhook/send-discord-webhook.command';
 
 @Injectable()
@@ -24,10 +25,10 @@ export class DiscordWebhookSagas {
         (event) =>
           new SendDiscordWebhookCommand({
             target: 'offer',
-            color: this.parseColor(event.businessFunction),
+            color: this.parseOfferColor(event.businessFunction),
             username: event.username,
             avatarURL: event.avatarURL,
-            title: this.parseTitle({
+            title: this.parseOfferTitle({
               businessFunction: event.businessFunction,
               title: event.title,
               price: event.price,
@@ -54,14 +55,36 @@ export class DiscordWebhookSagas {
       ),
     );
 
-  parseColor(businessFunction: string) {
+  @Saga()
+  userReviewCreated = (events$: Observable<any>): Observable<ICommand> =>
+    events$.pipe(
+      ofType(UserReviewCreatedEvent),
+      map(
+        (event) =>
+          new SendDiscordWebhookCommand({
+            target: 'userReview',
+            color: this.parseUserReviewColor(event.rating),
+            username: event.username,
+            avatarURL: event.userAvatarURL,
+            title: this.parseUserReviewTitle({ rating: event.rating, title: event.title }),
+            url: this.parseUserReviewURL({ slug: event.slug! }),
+          }),
+      ),
+    );
+
+  parseOfferColor(businessFunction: string) {
     if (businessFunction === 'sell') return 'Blue';
     if (businessFunction === 'buy') return 'DarkGreen';
     // SWAP
     return 'Orange';
   }
 
-  parseTitle({
+  parseUserReviewColor(rating: number) {
+    if (rating === 1) return 'Red';
+    return 'DarkGreen';
+  }
+
+  parseOfferTitle({
     businessFunction,
     title,
     price,
@@ -76,11 +99,20 @@ export class DiscordWebhookSagas {
     return `[교환합니다] ${title} ${price ? `- 내 추가금 +${price}` : ''}`;
   }
 
+  parseUserReviewTitle({ rating, title }: { rating: number; title: string }) {
+    if (rating === 1) return `[비매너 후기] ${title}`;
+    return `[매너 후기] ${title}`;
+  }
+
   parseOfferURL({ slug }: { slug: string }) {
     return `${this.configService.get('frontend.host')}/${OFFER}/${slug}`;
   }
 
   parseReportURL({ reportId }: { reportId: string }) {
     return `${this.configService.get('frontend.host')}/report/${reportId}`;
+  }
+
+  parseUserReviewURL({ slug }: { slug: string }) {
+    return `${this.configService.get('frontend.host')}/review/${slug}`;
   }
 }
