694939f39fb422e44ab2cdc9b0f7c0c307354ae7
keypport <keypport@gmail.com>
2024-05-16 01:39:23 +0900

    chore: remove unused import

--- apps/bot/src/events/sell-message-deleted/sell-message-deleted.handler.ts
+++ apps/bot/src/events/sell-message-deleted/sell-message-deleted.handler.ts
@@ -2,7 +2,6 @@ import { Injectable, UseGuards } from '@nestjs/common';
 import { Context, ContextOf, On } from 'necord';
 import { GroupGuard } from '@app/bot/apps/group/guards/group.guard';
 import { Type } from '@app/bot/decorators/type.decorator';
-import { OfferClient } from '@app/bot/apps/offer/clients/offer.client';
 import { ParseUserFromDeletedMessagePipe } from '@app/bot/apps/user/pipes/parse-user-from-deleted-message.pipe';
 import { MyUserResponse } from '@lib/domains/user/application/dtos/my-user.response';
 import { OfferChannelGuard } from '@app/bot/apps/offer/guards/offer-channel.guard';
