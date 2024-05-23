2508ab9491b730d6a64e302aca897d34d2548ce7
keypport <keypport@gmail.com>
2024-05-24 04:50:34 +0900

    feat: publish bidCanceled in cancel bid handler

--- libs/domains/src/auction/application/commands/cancel-bid/cancel-bid.handler.ts
+++ libs/domains/src/auction/application/commands/cancel-bid/cancel-bid.handler.ts
@@ -2,9 +2,11 @@ import { CommandHandler, ICommandHandler, EventPublisher } from '@nestjs/cqrs';
 import { Inject } from '@nestjs/common';
 import { AuctionErrorMessage } from '@lib/domains/auction/domain/auction.error.message';
 import { AuctionEntity } from '@lib/domains/auction/domain/auction.entity';
+import { GraphqlPubSub } from '@lib/shared/pubsub/graphql-pub-sub';
 import { CancelBidCommand } from './cancel-bid.command';
 import { AuctionLoadPort } from '../../ports/out/auction.load.port';
 import { BidSavePort } from '../../ports/out/bid.save.port';
+import { parseBidCanceledTriggerName } from '../../subscriptions/bid-canceled/parse-bid-canceled-trigger-name';
 
 @CommandHandler(CancelBidCommand)
 export class CancelBidHandler implements ICommandHandler<CancelBidCommand> {
@@ -19,7 +21,13 @@ export class CancelBidHandler implements ICommandHandler<CancelBidCommand> {
     if (!auction) throw new Error(AuctionErrorMessage.AUCTION_NOT_FOUND);
 
     auction = this.publisher.mergeObjectContext(new AuctionEntity(auction));
-    const bid = auction.cancelBid(command);
+    const bid = auction.cancelBid({ userId: command.user.id, bidId: command.bidId });
     await this.bidSavePort.cancelBid(bid);
+
+    await GraphqlPubSub.publish(parseBidCanceledTriggerName(auction.id), {
+      bidCanceled: {
+        id: bid.id,
+      },
+    });
   }
 }
