dd1123874d69db61459abacc913b4013278f81ca
keypport <keypport@gmail.com>
2024-05-25 17:26:46 +0900

    feat: add find auction interaction items args

---
+++ libs/domains/src/auction/application/queries/find-auction-interaction-items/find-auction-interaction-items-where.args.ts
@@ -0,0 +1,17 @@
+import { ArgsType, Field, ID } from '@nestjs/graphql';
+import { IsOptional } from 'class-validator';
+
+@ArgsType()
+export class FindAuctionInteractionItemsWhereArgs {
+  @IsOptional()
+  @Field(() => ID, { nullable: true })
+  auctionId?: string;
+
+  @IsOptional()
+  @Field(() => ID, { nullable: true })
+  userId?: string;
+
+  @IsOptional()
+  @Field(() => String, { nullable: true })
+  status?: string;
+}
