be4740a85f2e39735a24bce11967ff9225867ed6
keypport <keypport@gmail.com>
2024-06-01 02:01:25 +0900

    feat: add FindUserWithoutSocialAccountsCountHandler

--- libs/domains/src/user/application/queries/user.query.providers.ts
+++ libs/domains/src/user/application/queries/user.query.providers.ts
@@ -1,5 +1,6 @@
 import { FindAuthorHandler } from './find-author/find-author.handler';
 import { FindMyUserHandler } from './find-my-user/find-my-user.handler';
+import { FindUserWithoutSocialAccountsCountHandler } from './find-user-without-social-accounts-count/find-user-without-social-accounts-count.handler';
 import { FindUserHandler } from './find-user/find-user.handler';
 import { FindUsersHandler } from './find-users/find-users.handler';
 
@@ -8,4 +9,5 @@ export const USER_QUERY_PROVIDERS = [
   FindUsersHandler,
   FindMyUserHandler,
   FindAuthorHandler,
+  FindUserWithoutSocialAccountsCountHandler,
 ];
