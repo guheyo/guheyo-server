595dbe24f736ed5003eabd03e7b66dff6ee409ce
keypport <keypport@gmail.com>
2024-06-03 02:30:39 +0900

    feat: check kakao role, assign, send dm

--- apps/bot/src/events/kakao-role-requested/kakao-role-requested.handler.ts
+++ apps/bot/src/events/kakao-role-requested/kakao-role-requested.handler.ts
@@ -3,41 +3,45 @@ import { Context, ContextOf, On } from 'necord';
 import { GroupGuard } from '@app/bot/apps/group/guards/group.guard';
 import { ParseUserFromMessagePipe } from '@app/bot/apps/user/pipes/parse-user-from-message.pipe';
 import { MyUserResponse } from '@lib/domains/user/application/dtos/my-user.response';
-import { UserClient } from '@app/bot/apps/user/clients/user.client';
 import { CommandChannelGuard } from '@app/bot/apps/command/guards/command-channel.guard';
 import { Name } from '@app/bot/decorators/name.decorator';
 import { GuildName } from '@app/bot/decorators/guild-name.decorator';
+import { UserClient } from '@app/bot/apps/user/clients/user.client';
+import { KakaoService } from '@app/bot/apps/kakao/service/kakao.service';
 
 @GuildName('커스텀 키보드')
 @Name('kakao')
 @UseGuards(GroupGuard, CommandChannelGuard)
 @Injectable()
 export class KakaoRoleRequestedHandler {
-  constructor(private readonly userClient: UserClient) {}
+  constructor(
+    private readonly kakaoService: KakaoService,
+    private readonly userClient: UserClient,
+  ) {}
 
   @On('messageCreate')
   public async onRequestedKakaoRole(
-    @Context(ParseUserFromMessagePipe)
-    user: MyUserResponse,
-    @Context()
-    [message]: ContextOf<'messageCreate'>,
+    @Context(ParseUserFromMessagePipe) user: MyUserResponse,
+    @Context() [message]: ContextOf<'messageCreate'>,
   ) {
     const { member } = message;
-    if (!member) return;
-    if (!message.guild) return;
+    if (!member || !message.guild) return;
 
     const provider = 'kakao';
     const socialRole = this.userClient.findSocialRole(await message.guild.roles.fetch(), provider);
     if (!socialRole) return;
 
-    const memberHasRole = this.userClient.memberHasRoles(member, [socialRole]);
-    if (memberHasRole) {
-      // TODO: send confirm DM, log
+    if (this.userClient.memberHasRoles(member, [socialRole])) {
+      await this.kakaoService.sendCompletionDM(member);
+      return;
     }
+
     const socialAccount = this.userClient.findSocialAccount(user, provider);
     if (socialAccount) {
-      // TODO: assgin role, send success DM, log
+      await this.kakaoService.assignRoleAndNotify(member, socialRole);
+      return;
     }
-    // TODO: send kakao-auth link DM, log
+
+    await this.kakaoService.sendKakaoAuthLink(member);
   }
 }
