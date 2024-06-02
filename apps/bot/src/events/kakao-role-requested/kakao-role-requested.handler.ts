bb7e5b24e0b6f293088fe5471c4aae6192c53a3c
keypport <keypport@gmail.com>
2024-06-03 04:07:32 +0900

    feat: remove await in message.delete

--- apps/bot/src/events/kakao-role-requested/kakao-role-requested.handler.ts
+++ apps/bot/src/events/kakao-role-requested/kakao-role-requested.handler.ts
@@ -24,7 +24,7 @@ export class KakaoRoleRequestedHandler {
     @Context(ParseUserFromMessagePipe) user: MyUserResponse,
     @Context() [message]: ContextOf<'messageCreate'>,
   ) {
-    await message.delete();
+    message.delete();
 
     const { member } = message;
     if (!member || !message.guild) return;
