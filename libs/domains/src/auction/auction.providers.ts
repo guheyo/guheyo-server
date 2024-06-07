30a3411b2347ad040ff000a4ca1b55098121fe8d
keypport <keypport@gmail.com>
2024-06-08 02:46:05 +0900

    feat: import Auction services in Auction providers

--- libs/domains/src/auction/auction.providers.ts
+++ libs/domains/src/auction/auction.providers.ts
@@ -3,6 +3,7 @@ import { AUCTION_COMMAND_PROVIDERS } from './application/commands/auction.comman
 import { AUCTION_QUERY_PROVIDERS } from './application/queries/auction.query.providers';
 import { AUCTION_EVENT_PROVIDERS } from './application/events/auction.event.providers';
 import { AuctionSagas } from './application/sagas/auction.sagas';
+import { AUCTION_SERVICES } from './application/services/auction.services';
 
 export const AUCTION_PROVIDERS = [
   {
@@ -20,5 +21,6 @@ export const AUCTION_PROVIDERS = [
   ...AUCTION_COMMAND_PROVIDERS,
   ...AUCTION_QUERY_PROVIDERS,
   ...AUCTION_EVENT_PROVIDERS,
+  ...AUCTION_SERVICES,
   AuctionSagas,
 ];
