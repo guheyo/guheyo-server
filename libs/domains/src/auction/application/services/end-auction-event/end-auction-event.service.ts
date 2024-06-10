ddd2f13d9a76bb226dc54166bbb755180ad1cc38
keypport <keypport@gmail.com>
2024-06-10 15:03:58 +0900

    feat: trigger event after 1 minute

--- libs/domains/src/auction/application/services/end-auction-event/end-auction-event.service.ts
+++ libs/domains/src/auction/application/services/end-auction-event/end-auction-event.service.ts
@@ -31,18 +31,29 @@ export class EndAuctionEventService {
     const ruleName = this.eventBridgeService.getRuleName(prefixWithId);
     const ruleArn = this.eventBridgeService.getRuleArn(ruleName);
     const targetId = this.eventBridgeService.getTargetId(prefixWithId);
+    // Trigger Event after 1 minute
+    const delayedEndTime = this.eventBridgeService.getDelayedEndTime(endTime);
+    const scheduleExpression = this.eventBridgeService.generateCronExpression(delayedEndTime);
     const lambdaArn = this.lambdaService.getLambdaFunctionArn(this.functionName);
     const input = JSON.stringify({ auctionId, extendedEndDate: endTime });
     const statementId = this.lambdaService.getEventBridgeInvokeStatementId(prefixWithId);
 
-    await this.eventBridgeService.scheduleRule(ruleName, targetId, endTime, lambdaArn, input);
+    await this.eventBridgeService.scheduleRule(
+      ruleName,
+      targetId,
+      scheduleExpression,
+      lambdaArn,
+      input,
+    );
     await this.lambdaService.addPermission(this.functionName, statementId, ruleArn);
   }
 
   async updateEndAuctionEvent(auctionId: string, endTime: Date): Promise<void> {
     const prefixWithId = this.getPrefixWithId(auctionId);
     const ruleName = this.eventBridgeService.getRuleName(prefixWithId);
-    const scheuldExpression = this.eventBridgeService.generateCronExpression(endTime);
+    // Trigger Event after 1 minute
+    const delayedEndTime = this.eventBridgeService.getDelayedEndTime(endTime);
+    const scheuldExpression = this.eventBridgeService.generateCronExpression(delayedEndTime);
 
     await this.eventBridgeService.updateRuleSchedule(ruleName, scheuldExpression);
   }
