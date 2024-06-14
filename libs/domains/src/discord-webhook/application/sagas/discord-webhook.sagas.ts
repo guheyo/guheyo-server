0021c0cf2c3d7e36a91d80ad8b7e8b1c0ed2de42
keypport <keypport@gmail.com>
2024-06-15 04:59:01 +0900

    feat: pass embed to SendDiscordWebhookCommand, add auctionCreated in DiscordWebhookSagas

--- libs/domains/src/discord-webhook/application/sagas/discord-webhook.sagas.ts
+++ libs/domains/src/discord-webhook/application/sagas/discord-webhook.sagas.ts
@@ -7,6 +7,8 @@ import { includes } from 'lodash';
 import { ReportCreatedEvent } from '@lib/domains/report/application/events/report-created/report-created.event';
 import { UserReviewCreatedEvent } from '@lib/domains/user-review/application/events/user-review-created/user-review-created.event';
 import { AuctionCreatedEvent } from '@lib/domains/auction/application/events/auction-created/auction-created.event';
+import { EmbedBuilder } from 'discord.js';
+import dayjs from 'dayjs';
 import { SendDiscordWebhookCommand } from '../commands/send-discord-webhook/send-discord-webhook.command';
 import { DiscordWebhookParser } from '../services/discord-webhook.parser';
 
@@ -21,21 +23,26 @@ export class DiscordWebhookSagas {
       filter((event) => event.userAgent !== 'discord'),
       filter((event) => !!event.slug),
       filter((event) => includes(OFFER_BUSINESS_FUNCTIONS, event.businessFunction)),
-      map(
-        (event) =>
-          new SendDiscordWebhookCommand({
-            target: 'offer',
-            color: this.discordWebhookParser.parseOfferColor(event.businessFunction),
-            username: event.username,
-            avatarURL: event.avatarURL,
-            title: this.discordWebhookParser.parseOfferTitle({
+      map((event) => {
+        const embed = new EmbedBuilder()
+          .setTitle(
+            this.discordWebhookParser.parseOfferTitle({
               businessFunction: event.businessFunction,
               title: event.title,
               price: event.price,
             }),
-            url: this.discordWebhookParser.parseOfferURL({ slug: event.slug! }),
-          }),
-      ),
+          )
+          .setColor(this.discordWebhookParser.parseOfferColor(event.businessFunction))
+          .setAuthor({
+            name: event.username,
+            iconURL: event.avatarURL,
+          })
+          .setDescription(this.discordWebhookParser.parseOfferURL({ slug: event.slug! }));
+        return new SendDiscordWebhookCommand({
+          target: 'offer',
+          embed,
+        });
+      }),
     );
 
   @Saga()
@@ -44,53 +51,72 @@ export class DiscordWebhookSagas {
       ofType(AuctionCreatedEvent),
       filter((event) => event.userAgent !== 'discord'),
       filter((event) => !!event.slug),
-      map(
-        (event) =>
-          new SendDiscordWebhookCommand({
-            target: 'auction',
-            color: 'DarkGreen',
-            username: event.username,
-            avatarURL: event.avatarURL,
-            title: event.title,
-            url: this.discordWebhookParser.parseAuctionURL({ slug: event.slug! }),
-          }),
-      ),
+      map((event) => {
+        const unixTimestamp = Math.floor(event.extendedEndDate.getTime() / 1000);
+        const embed = new EmbedBuilder()
+          .setAuthor({
+            name: event.username,
+            iconURL: event.avatarURL || undefined,
+          })
+          .setColor('DarkGreen')
+          .setTitle(`${event.title}\n${process.env.FRONTEND_HOST}/auction/${event.slug}`)
+          .setDescription(
+            `경매 시작: ${dayjs(event.createdAt).format('YYYY-MM-DD HH:mm')}\n경매 종료: ${dayjs(
+              event.extendedEndDate,
+            ).format('YYYY-MM-DD HH:mm')}\n${`<t:${unixTimestamp}:R> 종료`}`,
+          )
+          .setFooter({
+            text: `입찰가: 0`,
+          });
+        return new SendDiscordWebhookCommand({
+          target: 'auction',
+          embed,
+        });
+      }),
     );
 
   @Saga()
   reportCreated = (events$: Observable<any>): Observable<ICommand> =>
     events$.pipe(
       ofType(ReportCreatedEvent),
-      map(
-        (event) =>
-          new SendDiscordWebhookCommand({
-            target: 'report',
-            color: 'Red',
-            username: event.reportedUserUsername,
-            avatarURL: event.reportedUserAvatarURL,
-            title: event.reason,
-            url: this.discordWebhookParser.parseReportURL({ reportId: event.reportId }),
-          }),
-      ),
+      map((event) => {
+        const embed = new EmbedBuilder()
+          .setTitle(event.reason)
+          .setColor('Red')
+          .setAuthor({
+            name: event.reportedUserUsername,
+            iconURL: event.reportedUserAvatarURL,
+          })
+          .setDescription(this.discordWebhookParser.parseReportURL({ reportId: event.reportId }));
+        return new SendDiscordWebhookCommand({
+          target: 'report',
+          embed,
+        });
+      }),
     );
 
   @Saga()
   userReviewCreated = (events$: Observable<any>): Observable<ICommand> =>
     events$.pipe(
       ofType(UserReviewCreatedEvent),
-      map(
-        (event) =>
-          new SendDiscordWebhookCommand({
-            target: 'userReview',
-            color: this.discordWebhookParser.parseUserReviewColor(event.rating),
-            username: event.username,
-            avatarURL: event.userAvatarURL,
-            title: this.discordWebhookParser.parseUserReviewTitle({
+      map((event) => {
+        const embed = new EmbedBuilder()
+          .setTitle(
+            this.discordWebhookParser.parseUserReviewTitle({
               rating: event.rating,
               title: event.title,
             }),
-            url: this.discordWebhookParser.parseUserReviewURL({ slug: event.slug! }),
-          }),
-      ),
+          )
+          .setColor(this.discordWebhookParser.parseUserReviewColor(event.rating))
+          .setAuthor({
+            name: event.username,
+            iconURL: event.userAvatarURL,
+          })
+          .setDescription(this.discordWebhookParser.parseUserReviewURL({ slug: event.slug! }));
+        return new SendDiscordWebhookCommand({
+          target: 'userReview',
+          embed,
+        });
+      }),
     );
 }
