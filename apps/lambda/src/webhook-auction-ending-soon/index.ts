8ae9a9d46330856c5f85547d0fe2d19ec42969c1
keypport <113260136+keypport@users.noreply.github.com>
2024-06-14 23:53:46 +0900

    feat: add webhook auction ending soon, connect elastic IP with eni of lambda security group (#272)
    * chore: remove ts-loader, add esbuild-loader
    
    * feat: change to esbuild-loader
    
    * chore: add ts-loader
    
    * chore: add @aws-sdk peer dependencies
    
    * refactor: move parse logics to DiscordWebhookParser
    
    * feat: import DiscordWebhookParser in DISCORD_WEBHOOK_PROVIDERS
    
    * feat: add parseAuctionURL
    
    * feat: add webhook-auction-ending-soon
    
    * feat: send webhook after update status
    
    * feat: handle when bids are not exist

--- apps/lambda/src/webhook-auction-ending-soon/index.ts
+++ apps/lambda/src/webhook-auction-ending-soon/index.ts
@@ -1,13 +1,12 @@
 import { EmbedBuilder, WebhookClient } from 'discord.js';
-import { AUCTION_CLOSED } from '@lib/domains/auction/domain/auction.constants';
 import { PrismaClient } from '@prisma/client';
 import dayjs from 'dayjs';
 
 export const handler = async (event: any): Promise<void> => {
-  const { auctionId, extendedEndDate } = event;
+  const { auctionId } = event;
 
-  if (!auctionId || !extendedEndDate) {
-    console.error('Invalid event data: auctionId and extendedEndDate are required');
+  if (!auctionId) {
+    console.error('Invalid event data: auctionId is required');
     return;
   }
 
@@ -49,41 +48,9 @@ export const handler = async (event: any): Promise<void> => {
 
     if (!auction) {
       console.error(`Auction with ID ${auctionId} not found.`);
-      return;
-    }
-
-    const currentDateTime = new Date();
-    const expectedEndDate = new Date(extendedEndDate);
-
-    // Check if the current time is at least the extended end date
-    if (currentDateTime < expectedEndDate) {
-      console.log(
-        `Current time (${currentDateTime}) is before the extended end date (${expectedEndDate}). No action taken.`,
-      );
       await prisma.$disconnect();
       return;
     }
-
-    // Check if the auction is already closed
-    if (auction.status === AUCTION_CLOSED) {
-      console.log(`Auction with ID ${auctionId} is already closed. No action taken.`);
-      await prisma.$disconnect();
-      return;
-    }
-
-    // Proceed with closing the auction
-    await prisma.auction.update({
-      where: {
-        id: auction.id,
-        version: auction.version,
-      },
-      data: {
-        version: {
-          increment: 1,
-        },
-        status: AUCTION_CLOSED,
-      },
-    });
 
     const webhookClient = new WebhookClient({ url: process.env.WEBHOOK_URL! });
 
@@ -91,30 +58,36 @@ export const handler = async (event: any): Promise<void> => {
       (account) => account.provider === 'discord',
     );
 
+    const unixTimestamp = Math.floor(auction.extendedEndDate.getTime() / 1000);
     const embed = new EmbedBuilder()
       .setAuthor({
         name: auction.post.user.username,
         iconURL: auction.post.user.avatarURL || undefined,
       })
-      .setColor('Red')
+      .setColor('DarkGreen')
       .setTitle(`${auction.post.title}\n${process.env.FRONTEND_HOST}/auction/${auction.post.slug}`)
       .setDescription(
         `판매자: ${
           discordAccount ? `<@${discordAccount.socialId}>` : auction.post.user.username
-        }\n경매 종료: ${dayjs(auction.extendedEndDate).format('YYYY-MM-DD HH:mm')}`,
+        }\n경매 종료: ${dayjs(auction.extendedEndDate).format(
+          'YYYY-MM-DD HH:mm',
+        )}\n${`<t:${unixTimestamp}:R> 종료`}`,
       )
       .setFooter({
-        text: `낙찰가: ${String(auction.bids[0]?.price || 0)}`,
+        text: `입찰가: ${String(auction.bids[0]?.price || 0)}`,
       });
 
     await webhookClient.send({ embeds: [embed] });
 
-    const endTime = new Date();
-    console.log(`Auction with ID ${auctionId} successfully ended at: ${endTime.toISOString()}`);
+    console.log(
+      `Webhook sent for Auction ${auctionId} with extended end date: ${dayjs(
+        auction.extendedEndDate,
+      ).format('YYYY-MM-DD HH:mm:ss')}`,
+    );
 
     await prisma.$disconnect();
   } catch (error) {
-    console.error(`Error ending auction with ID ${auctionId}:`, error);
+    console.error(`Error handling auction with ID ${auctionId}:`, error);
     throw error;
   }
 };
--- libs/domains/src/discord-webhook/application/sagas/discord-webhook.sagas.ts
+++ libs/domains/src/discord-webhook/application/sagas/discord-webhook.sagas.ts
@@ -2,17 +2,16 @@ import { Injectable } from '@nestjs/common';
 import { ICommand, Saga, ofType } from '@nestjs/cqrs';
 import { Observable, filter, map } from 'rxjs';
 import { OfferCreatedEvent } from '@lib/domains/offer/application/events/offer-created/offer-created.event';
-import { ConfigService } from '@nestjs/config';
 import { OFFER_BUSINESS_FUNCTIONS } from '@lib/domains/offer/domain/offer.types';
 import { includes } from 'lodash';
-import { OFFER } from '@lib/domains/offer/domain/offer.constants';
 import { ReportCreatedEvent } from '@lib/domains/report/application/events/report-created/report-created.event';
 import { UserReviewCreatedEvent } from '@lib/domains/user-review/application/events/user-review-created/user-review-created.event';
 import { SendDiscordWebhookCommand } from '../commands/send-discord-webhook/send-discord-webhook.command';
+import { DiscordWebhookParser } from '../services/discord-webhook.parser';
 
 @Injectable()
 export class DiscordWebhookSagas {
-  constructor(private readonly configService: ConfigService) {}
+  constructor(private readonly discordWebhookParser: DiscordWebhookParser) {}
 
   @Saga()
   offerCreated = (events$: Observable<any>): Observable<ICommand> =>
@@ -25,15 +24,15 @@ export class DiscordWebhookSagas {
         (event) =>
           new SendDiscordWebhookCommand({
             target: 'offer',
-            color: this.parseOfferColor(event.businessFunction),
+            color: this.discordWebhookParser.parseOfferColor(event.businessFunction),
             username: event.username,
             avatarURL: event.avatarURL,
-            title: this.parseOfferTitle({
+            title: this.discordWebhookParser.parseOfferTitle({
               businessFunction: event.businessFunction,
               title: event.title,
               price: event.price,
             }),
-            url: this.parseOfferURL({ slug: event.slug! }),
+            url: this.discordWebhookParser.parseOfferURL({ slug: event.slug! }),
           }),
       ),
     );
@@ -50,7 +49,7 @@ export class DiscordWebhookSagas {
             username: event.reportedUserUsername,
             avatarURL: event.reportedUserAvatarURL,
             title: event.reason,
-            url: this.parseReportURL({ reportId: event.reportId }),
+            url: this.discordWebhookParser.parseReportURL({ reportId: event.reportId }),
           }),
       ),
     );
@@ -63,56 +62,15 @@ export class DiscordWebhookSagas {
         (event) =>
           new SendDiscordWebhookCommand({
             target: 'userReview',
-            color: this.parseUserReviewColor(event.rating),
+            color: this.discordWebhookParser.parseUserReviewColor(event.rating),
             username: event.username,
             avatarURL: event.userAvatarURL,
-            title: this.parseUserReviewTitle({ rating: event.rating, title: event.title }),
-            url: this.parseUserReviewURL({ slug: event.slug! }),
+            title: this.discordWebhookParser.parseUserReviewTitle({
+              rating: event.rating,
+              title: event.title,
+            }),
+            url: this.discordWebhookParser.parseUserReviewURL({ slug: event.slug! }),
           }),
       ),
     );
-
-  parseOfferColor(businessFunction: string) {
-    if (businessFunction === 'sell') return 'Blue';
-    if (businessFunction === 'buy') return 'DarkGreen';
-    // SWAP
-    return 'Orange';
-  }
-
-  parseUserReviewColor(rating: number) {
-    if (rating === 1) return 'Red';
-    return 'DarkGreen';
-  }
-
-  parseOfferTitle({
-    businessFunction,
-    title,
-    price,
-  }: {
-    businessFunction: string;
-    title: string;
-    price: number;
-  }) {
-    if (businessFunction === 'sell') return `[삽니다] ${title} - ${price}`;
-    if (businessFunction === 'buy') return `[삽니다] ${title} - ${price}`;
-    // swap
-    return `[교환합니다] ${title} ${price ? `- 내 추가금 +${price}` : ''}`;
-  }
-
-  parseUserReviewTitle({ rating, title }: { rating: number; title: string }) {
-    if (rating === 1) return `[비매너 후기] ${title}`;
-    return `[매너 후기] ${title}`;
-  }
-
-  parseOfferURL({ slug }: { slug: string }) {
-    return `${this.configService.get('frontend.host')}/${OFFER}/${slug}`;
-  }
-
-  parseReportURL({ reportId }: { reportId: string }) {
-    return `${this.configService.get('frontend.host')}/report/${reportId}`;
-  }
-
-  parseUserReviewURL({ slug }: { slug: string }) {
-    return `${this.configService.get('frontend.host')}/review/${slug}`;
-  }
 }
