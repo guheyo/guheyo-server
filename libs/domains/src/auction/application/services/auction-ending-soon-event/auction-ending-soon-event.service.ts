6833508a1c87bfce324010a65afff26c08da10ad
keypport <keypport@gmail.com>
2024-06-15 01:37:53 +0900

    feat: add AuctionEndingSoonEvent Service

---
+++ libs/domains/src/auction/application/services/auction-ending-soon-event/auction-ending-soon-event.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@nestjs/common';
+import { EventBridgeService } from '@lib/shared/aws/event-bridge/event-bridge.service';
+import { LambdaService } from '@lib/shared/aws/lambda/lambda.service';
+
+@Injectable()
+export class AuctionEndingSoonEventService {
+  private readonly functionName: string = 'auction-ending-soon';
+
+  private readonly prefix: string = `${process.env.NODE_ENV}-${this.functionName}`;
+
+  constructor(
+    private readonly eventBridgeService: EventBridgeService,
+    private readonly lambdaService: LambdaService,
+  ) {}
+
+  private getPrefixWithId(auctionId: string): string {
+    return `${this.prefix}-${auctionId}`;
+  }
+
+  async scheduleAuctionEndingSoonEvent(auctionId: string, endTime: Date): Promise<void> {
+    const prefixWithId = this.getPrefixWithId(auctionId);
+    const ruleName = this.eventBridgeService.getRuleName(prefixWithId);
+    const ruleArn = this.eventBridgeService.getRuleArn(ruleName);
+    const targetId = this.eventBridgeService.getTargetId(prefixWithId);
+    // Trigger Event before 1 hour
+    const oneHourBeforeEndTime = this.eventBridgeService.getDelayedEndTime(
+      endTime,
+      -1 * 60 * 60000,
+    );
+    const scheduleExpression = this.eventBridgeService.generateCronExpression(oneHourBeforeEndTime);
+    const lambdaArn = this.lambdaService.getLambdaFunctionArn(this.functionName);
+    const input = JSON.stringify({ auctionId, extendedEndDate: endTime });
+    const statementId = this.lambdaService.getEventBridgeInvokeStatementId(prefixWithId);
+
+    await this.eventBridgeService.scheduleRule({
+      ruleName,
+      targetId,
+      scheduleExpression,
+      lambdaArn,
+      input,
+    });
+    await this.lambdaService.addPermission({
+      functionName: this.functionName,
+      statementId,
+      ruleArn,
+    });
+  }
+}
--- libs/domains/src/auction/application/services/auction.services.ts
+++ libs/domains/src/auction/application/services/auction.services.ts
@@ -1,3 +1,4 @@
-import { EndAuctionEventService } from './end-auction-event/end-auction-event.service';
+import { AuctionEndEventService } from './auction-end-event/auction-end-event.service';
+import { AuctionEndingSoonEventService } from './auction-ending-soon-event/auction-ending-soon-event.service';
 
-export const AUCTION_SERVICES = [EndAuctionEventService];
+export const AUCTION_SERVICES = [AuctionEndEventService, AuctionEndingSoonEventService];
