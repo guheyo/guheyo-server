99ee4d8570b7ae1b20e55f4f076afad6750ca5a8
keypport <keypport@gmail.com>
2024-06-14 22:59:07 +0900

    feat: import DiscordWebhookParser in DISCORD_WEBHOOK_PROVIDERS

--- libs/domains/src/discord-webhook/discord-webhook.providers.ts
+++ libs/domains/src/discord-webhook/discord-webhook.providers.ts
@@ -1,9 +1,11 @@
 import { DiscordWebhookService } from '@lib/shared/discord/discord-webhook.service';
 import { DISCORD_WEBHOOK_COMMAND_PROVIDERS } from './application/commands/discord-webhook.command.providers';
 import { DiscordWebhookSagas } from './application/sagas/discord-webhook.sagas';
+import { DiscordWebhookParser } from './application/services/discord-webhook.parser';
 
 export const DISCORD_WEBHOOK_PROVIDERS = [
   ...DISCORD_WEBHOOK_COMMAND_PROVIDERS,
   DiscordWebhookSagas,
   DiscordWebhookService,
+  DiscordWebhookParser,
 ];
