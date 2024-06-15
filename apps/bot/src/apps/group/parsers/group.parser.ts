66d764e284f9eb1c202a50ab7121b05564e97722
keypport <keypport@gmail.com>
2024-06-16 03:58:04 +0900

    feat: move parseCategoryId(categoryName from OfferParser to GroupParser, add parseCategoryNameFromMessage in OfferParser

--- apps/bot/src/apps/group/parsers/group.parser.ts
+++ apps/bot/src/apps/group/parsers/group.parser.ts
@@ -1,6 +1,9 @@
 import { Parser } from '@app/bot/shared/parsers/parser';
+import { GroupResponse } from '@lib/domains/group/application/dtos/group.response';
 import { TagResponse } from '@lib/domains/tag/application/dtos/tag.response';
 import { Injectable } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
+import { GroupErrorMessage } from './group.error-message';
 
 @Injectable()
 export class GroupParser extends Parser {
@@ -20,5 +23,12 @@ export class GroupParser extends Parser {
 
   parseTagIds(tagNames: string[], tags: TagResponse[]): string[] {
     return tags.filter((tag) => tagNames.includes(tag.name)).map((tag) => tag.id);
+  }
+
+  parseCategoryId(categoryName: string, group: GroupResponse): string {
+    const category = group.categories.find((c) => c.name === categoryName);
+
+    if (!category) throw new RpcException(GroupErrorMessage.NOT_FOUND_CATEGORY);
+    return category.id;
   }
 }
