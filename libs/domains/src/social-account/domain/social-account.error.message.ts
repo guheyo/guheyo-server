1fe11797e70c6d3e4a80a9eb1ec916cb04528ddc
keypport <keypport@gmail.com>
2024-06-15 21:21:41 +0900

    feat: add ACCESS_DENIED_PROVIDER in SocialAccountErrorMessage

--- libs/domains/src/social-account/domain/social-account.error.message.ts
+++ libs/domains/src/social-account/domain/social-account.error.message.ts
@@ -1,4 +1,5 @@
 export enum SocialAccountErrorMessage {
   SOCIAL_ACCOUNT_IS_NOT_FOUND = 'Social account is not found',
   SOCIAL_ACCOUNT_ALREADY_EXISTS = 'Social account already exists',
+  ACCESS_DENIED_PROVIDER = 'Access denied: Social account provider not allowed',
 }
