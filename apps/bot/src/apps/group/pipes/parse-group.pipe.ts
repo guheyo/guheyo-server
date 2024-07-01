e654a1496df3f93a88493aa253b5047f893f914c
keypport <113260136+keypport@users.noreply.github.com>
2024-07-01 21:59:32 +0900

    feat: implement ArticleCreatedHandler (#315)
    * feat: set channelId to channel.parentId if channel is instance of ThreadChannel
    
    * feat: add CommunityChannelGuard
    
    * fix: change channelName to threadChannel.parent.name
    
    * feat: add ArticleCreated Handler

--- apps/bot/src/apps/group/pipes/parse-group.pipe.ts
+++ apps/bot/src/apps/group/pipes/parse-group.pipe.ts
@@ -1,8 +1,10 @@
 import { ArgumentMetadata, Injectable, Logger, PipeTransform } from '@nestjs/common';
 import { ContextOf } from 'necord';
 import { GroupResponse } from '@lib/domains/group/application/dtos/group.response';
-import { Message } from 'discord.js';
+import { Message, ThreadChannel } from 'discord.js';
+import { RpcException } from '@nestjs/microservices';
 import { GroupClient } from '../clients/group.client';
+import { GroupErrorMessage } from '../parsers/group.error-message';
 
 @Injectable()
 export class ParseGroupPipe implements PipeTransform {
@@ -18,7 +20,12 @@ export class ParseGroupPipe implements PipeTransform {
 
     if (newMessage) message = await newMessage.fetch();
     else message = await createdOrOldmessage.fetch();
+    const { channel } = message;
 
-    return this.groupClient.fetchGroupByChannelId(message.channelId);
+    const isThreadChannel = channel instanceof ThreadChannel;
+    const channelId = isThreadChannel ? channel.parentId : channel.id;
+    if (!channelId) throw new RpcException(GroupErrorMessage.NOT_FOUND_CHANNEL);
+
+    return this.groupClient.fetchGroupByChannelId(channelId);
   }
 }
