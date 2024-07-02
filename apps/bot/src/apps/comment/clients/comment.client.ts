8d93499ce585da1aa18d71a520e7f2e7bd95c289
keypport <113260136+keypport@users.noreply.github.com>
2024-07-02 16:54:14 +0900

    feat: save comment image (#317)
    * feat: upload comment image
    
    * feat: upload and create attachments in createCommentsFromMessageWithUsers

--- apps/bot/src/apps/comment/clients/comment.client.ts
+++ apps/bot/src/apps/comment/clients/comment.client.ts
@@ -4,6 +4,7 @@ import { CreateCommentCommand } from '@lib/domains/comment/application/commands/
 import { CreateCommentsCommand } from '@lib/domains/comment/application/commands/create-comments/create-comments.command';
 import { CreateCommentInput } from '@lib/domains/comment/application/commands/create-comment/create-comment.input';
 import { Message } from 'discord.js';
+import { COMMENT } from '@lib/domains/comment/domain/comment.constants';
 import { UserImageClient } from '../../user-image/clients/user-image.client';
 import { CommentParser } from '../parsers/comment.parser';
 import { MessageWithUser } from '../../user/interfaces/user.interfaces';
@@ -25,14 +26,33 @@ export class CommentClient extends UserImageClient {
   }
 
   async createCommentsFromMessageWithUsers(messageWithUsers: MessageWithUser[]) {
+    const uploadPromises = messageWithUsers.map(async (messageWithUser) => {
+      const uploadUserImageInputList = this.userImageParser.parseUploadUserImageInputList(
+        messageWithUser.message,
+        COMMENT,
+      );
+      return this.uploadAndCreateAttachments(
+        messageWithUser.user,
+        uploadUserImageInputList,
+        COMMENT,
+      );
+    });
+    await Promise.all(uploadPromises);
+
     const commentCommands = this.commentParser.parseCreateCommentCommands(messageWithUsers);
     await this.createComments(commentCommands);
     this.logger.log(`comment<#${commentCommands.length}> created`);
   }
 
   async createCommentFromMessage(message: Message, user: MyUserResponse) {
-    const input = this.commentParser.parseCreateCommentInput(message);
-    await this.createComment({ input, user });
-    this.logger.log(`comment<#${input.id}> created`);
+    const uploadUserImageInputList = this.userImageParser.parseUploadUserImageInputList(
+      message,
+      COMMENT,
+    );
+    const createCommentInput = this.commentParser.parseCreateCommentInput(message);
+
+    await this.uploadAndCreateAttachments(user, uploadUserImageInputList, COMMENT);
+    await this.createComment({ input: createCommentInput, user });
+    this.logger.log(`comment<#${createCommentInput.id}> created`);
   }
 }
