4c500816ecb4921b09e2279c7418bba92b1a5220
keypport <keypport@gmail.com>
2024-07-07 19:31:12 +0900

    feat: add updateThreadFromPost

--- apps/bot/src/apps/thread/clients/thread.client.ts
+++ apps/bot/src/apps/thread/clients/thread.client.ts
@@ -5,6 +5,8 @@ import { ThreadPost } from '@app/bot/shared/interfaces/post-message.interfaces';
 import { CreateThreadInput } from '@lib/domains/thread/application/commands/create-thread/create-thread.input';
 import { CreateThreadCommand } from '@lib/domains/thread/application/commands/create-thread/create-thread.command';
 import { THREAD } from '@lib/domains/thread/domain/thread.constants';
+import { UpdateThreadInput } from '@lib/domains/thread/application/commands/update-thread/update-thread.input';
+import { UpdateThreadCommand } from '@lib/domains/thread/application/commands/update-thread/update-thread.command';
 import { UserImageClient } from '../../user-image/clients/user-image.client';
 import { ThreadParser } from '../parsers/thread.parser';
 
@@ -20,6 +22,10 @@ export class ThreadClient extends UserImageClient {
     await this.commandBus.execute(new CreateThreadCommand({ input, user }));
   }
 
+  async updateThread({ input, user }: { input: UpdateThreadInput; user: MyUserResponse }) {
+    await this.commandBus.execute(new UpdateThreadCommand({ input, user }));
+  }
+
   async createThreadFromPost(user: MyUserResponse, threadPost: ThreadPost, group: GroupResponse) {
     const uploadUserImageInputList = this.userImageParser.parseUploadUserImageInputList(
       threadPost.starterMessage,
@@ -30,5 +36,11 @@ export class ThreadClient extends UserImageClient {
     await this.uploadAndCreateAttachments(user, uploadUserImageInputList, THREAD);
     await this.createThread({ input: createThreadInput, user });
     this.logger.log(`thread<@${createThreadInput.id}> created`);
+  }
+
+  async updateThreadFromPost(user: MyUserResponse, threadPost: ThreadPost) {
+    const updateThreadInput = this.threadParser.parseUpdateThreadInput(threadPost);
+    await this.updateThread({ input: updateThreadInput, user });
+    this.logger.log(`thread <@${updateThreadInput.id}> updated`);
   }
 }
