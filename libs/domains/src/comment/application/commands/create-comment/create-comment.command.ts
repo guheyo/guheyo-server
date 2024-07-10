177a5049aefb8bc6ad31e74db9b620f7fabd3140
keypport <keypport@gmail.com>
2024-07-10 18:26:48 +0900

    feat: add userAgent in CreateCommentCommand

--- libs/domains/src/comment/application/commands/create-comment/create-comment.command.ts
+++ libs/domains/src/comment/application/commands/create-comment/create-comment.command.ts
@@ -15,12 +15,23 @@ export class CreateCommentCommand implements ICommand {
 
   user: MyUserResponse;
 
-  constructor({ input, user }: { input: CreateCommentInput; user: MyUserResponse }) {
+  userAgent?: string;
+
+  constructor({
+    input,
+    user,
+    userAgent,
+  }: {
+    input: CreateCommentInput;
+    user: MyUserResponse;
+    userAgent?: string;
+  }) {
     this.id = input.id;
     this.createdAt = input.createdAt;
     this.updatedAt = input.updatedAt;
     this.postId = input.postId;
     this.content = input.content;
     this.user = user;
+    this.userAgent = userAgent;
   }
 }
