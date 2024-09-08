94bab1f634ef919b765fea5c7b74fd2b420beb27
keypport <keypport@gmail.com>
2024-09-08 23:00:34 +0900

    feat: add BrandErrorMessage

---
+++ libs/domains/src/brand/domain/brand.error.message.ts
@@ -0,0 +1,7 @@
+export enum BrandErrorMessage {
+  BRAND_NOT_FOUND = 'Brand not found',
+  BRAND_ALREADY_EXISTS = 'Brand already exists',
+  INVALID_FIND_BRAND_ARGS = 'Invalid find brand args',
+  BRAND_CREATION_FAILED = 'Brand creation failed',
+  BRAND_UPDATE_REQUEST_FROM_UNAUTHORIZED_USER = 'Brand update request from unauthorized user',
+}
