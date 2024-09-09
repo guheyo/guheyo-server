894eaebe09c8f75803441d2d070b177e0823239f
keypport <keypport@gmail.com>
2024-09-10 01:13:33 +0900

    feat: add createBrand in BrandResolver

--- apps/api/src/app/brand/brand.resolver.ts
+++ apps/api/src/app/brand/brand.resolver.ts
@@ -4,12 +4,17 @@ import { UseGuards } from '@nestjs/common';
 import { BrandResponse } from '@lib/domains/brand/application/dtos/brand.response';
 import { FindBrandsQuery } from '@lib/domains/brand/application/queries/find-brands/find-brands.query';
 import { AuthenticatedSocialAccountAndRole } from '@lib/domains/auth/decorators/authenticated-social-account-and-role/authenticated-social-account-and-role.decorator';
-import { ROOT_BLOCKLIST_ROLE_NAMES } from '@lib/domains/role/domain/role.types';
+import {
+  ROOT_ADMIN_ROLE_NAMES,
+  ROOT_BLOCKLIST_ROLE_NAMES,
+} from '@lib/domains/role/domain/role.types';
 import { FollowBrandInput } from '@lib/domains/brand/application/commands/follow-brand/follow-brand.input';
 import { ExtractedUser } from '@lib/domains/auth/decorators/extracted-user/extracted-user.decorator';
 import { MyUserResponse } from '@lib/domains/user/application/dtos/my-user.response';
 import { FollowBrandCommand } from '@lib/domains/brand/application/commands/follow-brand/follow-brand.command';
 import { UnfollowBrandInput } from '@lib/domains/brand/application/commands/unfollow-brand/unfollow-brand.input';
+import { CreateBrandCommand } from '@lib/domains/brand/application/commands/create-brand/create-brand.command';
+import { CreateBrandInput } from '@lib/domains/brand/application/commands/create-brand/create-brand.input';
 import { GqlThrottlerBehindProxyGuard } from '../throttler/gql-throttler-behind-proxy.guard';
 
 @UseGuards(GqlThrottlerBehindProxyGuard)
@@ -24,6 +29,20 @@ export class BrandResolver {
   async findBrands(): Promise<BrandResponse[]> {
     const query = new FindBrandsQuery();
     return this.queryBus.execute(query);
+  }
+
+  @AuthenticatedSocialAccountAndRole({
+    providers: ['kakao'],
+    blocklistRoleNames: [...ROOT_BLOCKLIST_ROLE_NAMES],
+    allowlistRoleNames: [...ROOT_ADMIN_ROLE_NAMES],
+  })
+  @Mutation(() => BrandResponse)
+  async createBrand(
+    @Args('input') input: CreateBrandInput,
+    @ExtractedUser() user: MyUserResponse,
+  ): Promise<string> {
+    const brand = await this.commandBus.execute(new CreateBrandCommand({ input, user }));
+    return brand;
   }
 
   @AuthenticatedSocialAccountAndRole({
