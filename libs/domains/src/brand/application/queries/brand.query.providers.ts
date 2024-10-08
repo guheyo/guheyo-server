db0921b4989367c3e6e21509b0bc409aaa4eed25
keypport <keypport@gmail.com>
2024-10-09 01:18:49 +0900

    feat: add FindBrandPreviewHandler in BRAND_QUERY_PROVIDERS

--- libs/domains/src/brand/application/queries/brand.query.providers.ts
+++ libs/domains/src/brand/application/queries/brand.query.providers.ts
@@ -1,4 +1,5 @@
+import { FindBrandPreviewHandler } from './find-brand-preview/find-brand-preview.handler';
 import { FindBrandHandler } from './find-brand/find-brand.handler';
 import { FindBrandsHandler } from './find-brands/find-brands.handler';
 
-export const BRAND_QUERY_PROVIDERS = [FindBrandHandler, FindBrandsHandler];
+export const BRAND_QUERY_PROVIDERS = [FindBrandHandler, FindBrandsHandler, FindBrandPreviewHandler];
