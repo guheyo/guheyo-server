4e5fc55b8c25e70ddafaa77c594efe2ce43dc1fe
keypport <keypport@gmail.com>
2024-10-14 03:26:23 +0900

    feat: update BumpedEvent

--- libs/domains/src/bump/application/events/bumped/bumped.event.ts
+++ libs/domains/src/bump/application/events/bumped/bumped.event.ts
@@ -9,20 +9,60 @@ export class BumpedEvent implements IEvent {
 
   newPrice: number;
 
+  username: string;
+
+  avatarURL?: string;
+
+  businessFunction: string;
+
+  title: string;
+
+  slug?: string;
+
+  price: number;
+
+  postId: string;
+
+  thumbnail?: string;
+
   constructor({
     id,
     offerId,
     oldPrice,
     newPrice,
+    username,
+    avatarURL,
+    businessFunction,
+    title,
+    slug,
+    price,
+    postId,
+    thumbnail,
   }: {
     id: string;
     offerId: string;
     oldPrice: number;
     newPrice: number;
+    username: string;
+    avatarURL?: string;
+    businessFunction: string;
+    title: string;
+    slug?: string;
+    price: number;
+    postId: string;
+    thumbnail?: string;
   }) {
     this.id = id;
     this.offerId = offerId;
     this.oldPrice = oldPrice;
     this.newPrice = newPrice;
+    this.username = username;
+    this.avatarURL = avatarURL;
+    this.businessFunction = businessFunction;
+    this.title = title;
+    this.slug = slug;
+    this.price = price;
+    this.postId = postId;
+    this.thumbnail = thumbnail;
   }
 }
