b0dfd65399878ed8b7c4d6254590929e3bf8d179
keypport <keypport@gmail.com>
2024-10-15 22:02:59 +0900

    feat: add status, createdAt in FindSocialAccountConflictsWhereInput

--- libs/domains/src/social-account/application/queries/find-social-account-conflicts/find-social-account-conflicts-where.input.ts
+++ libs/domains/src/social-account/application/queries/find-social-account-conflicts/find-social-account-conflicts-where.input.ts
@@ -1,5 +1,6 @@
 import { Field, ID, InputType } from '@nestjs/graphql';
 import { IsOptional } from 'class-validator';
+import { GraphQLJSON } from 'graphql-type-json';
 
 @InputType()
 export class FindSocialAccountConflictsWhereInput {
@@ -10,4 +11,14 @@ export class FindSocialAccountConflictsWhereInput {
   @IsOptional()
   @Field(() => Boolean, { nullable: true })
   followed?: boolean;
+
+  @IsOptional()
+  @Field(() => String, { nullable: true })
+  status?: string;
+
+  @IsOptional()
+  @Field(() => GraphQLJSON, { nullable: true })
+  createdAt?: {
+    gt: string;
+  };
 }
