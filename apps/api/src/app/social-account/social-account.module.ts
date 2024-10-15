eea45c93ccb057af2759b7053f58a2e2a57c6fca
keypport <keypport@gmail.com>
2024-10-15 19:35:31 +0900

    feat: add SocialAccountResolver in SocialAccountModeul

--- apps/api/src/app/social-account/social-account.module.ts
+++ apps/api/src/app/social-account/social-account.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { PrismaModule } from '@lib/shared/prisma/prisma.module';
 import { SOCIAL_ACCOUNT_PROVIDERS } from '@lib/domains/social-account/social-account.providers';
+import { SocialAccountResolver } from './social-account.resolver';
 
 @Module({
   imports: [CqrsModule, PrismaModule],
-  // No SocialAccountResolver
-  providers: [...SOCIAL_ACCOUNT_PROVIDERS],
+  providers: [SocialAccountResolver, ...SOCIAL_ACCOUNT_PROVIDERS],
 })
 export class SocialAccountModule {}
