3d07c31192de57b0a3477eca86776219878e28d6
keypport <keypport@gmail.com>
2024-11-06 21:37:12 +0900

    feat: update parsePlatformName

--- libs/domains/src/platform/application/services/platform.service.ts
+++ libs/domains/src/platform/application/services/platform.service.ts
@@ -1,6 +1,6 @@
 export class PlatformService {
   parsePlatformName(url: string) {
-    if (/^https:\/\/smartstore\.naver\.com(\/|$)/.test(url)) return '스마트스토어';
+    if (/https?:\/\/(www\.)?smartstore\.naver\.com(\/|$)/.test(url)) return '스마트스토어';
     if (/https?:\/\/(www\.)?instagram\.com(\/|$)/.test(url)) return '인스타그램';
     if (/https?:\/\/(www\.)?discord\.com(\/|$)/.test(url)) return '디스코드';
     if (/https?:\/\/(www\.)?x\.com(\/|$)/.test(url)) return 'X';
