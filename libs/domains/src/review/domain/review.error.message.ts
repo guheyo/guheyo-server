28be55871f7604086cf779fb8d65a0ae312abc72
keypport <keypport@gmail.com>
2024-11-11 19:13:40 +0900

    feat: add FIND_REQUEST_FROM_UNAUTHORIZED_USER

--- libs/domains/src/review/domain/review.error.message.ts
+++ libs/domains/src/review/domain/review.error.message.ts
@@ -3,4 +3,5 @@ export enum ReviewErrorMessage {
   REVIEW_ALREADY_EXISTS = 'Review already exists',
   REVIEW_CREATION_FAILED = 'Review creation failed',
   REVIEW_UPDATE_REQUEST_FROM_UNAUTHORIZED_USER = 'Review update request from unauthorized user',
+  FIND_REQUEST_FROM_UNAUTHORIZED_USER = 'Find request from unauthorized user',
 }
