75081e2ad2c5931e5104c4e9bd33f763605d59d1
keypport <keypport@gmail.com>
2024-11-12 22:35:47 +0900

    feat: extends PrismaCommandHandler in CreateGroupHandler

--- libs/domains/src/group/application/commands/create-group/create-group.handler.ts
+++ libs/domains/src/group/application/commands/create-group/create-group.handler.ts
@@ -1,12 +1,16 @@
-import { CommandHandler, ICommandHandler } from '@nestjs/cqrs/dist';
+import { CommandHandler } from '@nestjs/cqrs/dist';
 import { Inject } from '@nestjs/common';
 import { GroupEntity } from '@lib/domains/group/domain/group.entity';
+import { PrismaCommandHandler } from '@lib/shared/cqrs/commands/handlers/prisma-command.handler';
 import { CreateGroupCommand } from './create-group.command';
 import { GroupSavePort } from '../../ports/out/group.save.port';
+import { GroupResponse } from '../../dtos/group.response';
 
 @CommandHandler(CreateGroupCommand)
-export class CreateGroupHandler implements ICommandHandler<CreateGroupCommand> {
-  constructor(@Inject('GroupSavePort') private savePort: GroupSavePort) {}
+export class CreateGroupHandler extends PrismaCommandHandler<CreateGroupCommand, GroupResponse> {
+  constructor(@Inject('GroupSavePort') private savePort: GroupSavePort) {
+    super(GroupResponse);
+  }
 
   async execute(command: CreateGroupCommand): Promise<void> {
     const group = new GroupEntity(command);
