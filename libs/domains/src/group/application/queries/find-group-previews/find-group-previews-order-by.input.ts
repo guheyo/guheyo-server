7420690c10380d19b3d7095cb7115e94edbd24c7
keypport <keypport@gmail.com>
2025-03-02 07:08:47 +0900

    feat: add FindGroupPreviewsWhereInput, FindGroupPreviewsOrderByInput

---
+++ libs/domains/src/group/application/queries/find-group-previews/find-group-previews-order-by.input.ts
@@ -0,0 +1,13 @@
+import { Field, InputType } from '@nestjs/graphql';
+import { IsOptional } from 'class-validator';
+
+@InputType()
+export class FindGroupPreviewsOrderByInput {
+  @IsOptional()
+  @Field(() => String, { nullable: true })
+  position?: 'asc' | 'desc';
+
+  @IsOptional()
+  @Field(() => String, { nullable: true })
+  createdAt?: 'asc' | 'desc';
+}
