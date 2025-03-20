4646cc838e5a641c5d5aa376c96a9ce7cbf0045b
keypport <keypport@gmail.com>
2025-03-21 02:20:57 +0900

    feat: change reportCommented to reportCommentCreated in ReportSagas

--- libs/domains/src/report/application/sagas/report.sagas.ts
+++ libs/domains/src/report/application/sagas/report.sagas.ts
@@ -1,22 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { ICommand, Saga, ofType } from '@nestjs/cqrs';
-import { Observable, concatMap, map, of } from 'rxjs';
+import { Observable, concatMap, of } from 'rxjs';
 import { ConnectRolesCommand } from '@lib/domains/user/application/commands/connect-roles/connect-roles.command';
 import { REPORTED_USER_ROLE_NAME } from '@lib/domains/role/domain/role.constants';
-import { CheckReportCommentsCommand } from '../commands/check-report-comments/check-report-comments.command';
 import { ReportCreatedEvent } from '../events/report-created/report-created.event';
-import { ReportCommentedEvent } from '../events/report-commented/report-commented.event';
+import { ReportCommentCreatedEvent } from '../events/report-comment-created/report-comment-created.event';
 import { ReportCommentUpdatedEvent } from '../events/report-comment-updated/report-comment-updated.event';
 
 @Injectable()
 export class ReportSagas {
-  @Saga()
-  reportCommented = (events$: Observable<any>): Observable<ICommand> =>
-    events$.pipe(
-      ofType(ReportCommentedEvent),
-      map((event) => new CheckReportCommentsCommand(event)),
-    );
-
   @Saga()
   reportCreated = (events$: Observable<any>): Observable<ICommand> =>
     events$.pipe(
@@ -32,6 +24,13 @@ export class ReportSagas {
           }),
         ),
       ),
+    );
+
+  @Saga()
+  reportCommentCreated = (events$: Observable<any>): Observable<ICommand> =>
+    events$.pipe(
+      ofType(ReportCommentCreatedEvent),
+      // TODO
     );
 
   @Saga()
