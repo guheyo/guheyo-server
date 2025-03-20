0988c8978517d25ef7f350f6a27202712c07430e
keypport <keypport@gmail.com>
2025-03-21 02:15:28 +0900

    feat: remove CheckReportCommentsHandler

--- libs/domains/src/report/application/commands/report.command.providers.ts
+++ libs/domains/src/report/application/commands/report.command.providers.ts
@@ -1,11 +1,9 @@
-import { CheckReportCommentsHandler } from './check-report-comments/check-report-comments.handler';
 import { CommentReportHandler } from './comment-report/comment-report.handler';
 import { CreateReportHandler } from './create-report/create-report.handler';
 import { UpdateReportCommentHandler } from './update-report-comment/update-report-comment.handler';
 
 export const REPORT_COMMAND_PROVIDERS = [
   CreateReportHandler,
-  CheckReportCommentsHandler,
   CommentReportHandler,
   UpdateReportCommentHandler,
 ];
