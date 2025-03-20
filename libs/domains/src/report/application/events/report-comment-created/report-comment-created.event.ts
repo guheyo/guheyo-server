af32aef86641fc76e6a254b28e0fc52573de891b
keypport <keypport@gmail.com>
2025-03-21 02:17:45 +0900

    feat: add content in ReportCommentCreatedInput

--- libs/domains/src/report/application/events/report-comment-created/report-comment-created.event.ts
+++ libs/domains/src/report/application/events/report-comment-created/report-comment-created.event.ts
@@ -4,7 +4,10 @@ import { ReportCommentCreatedInput } from './report-comment-created.input';
 export class ReportCommentCreatedEvent implements IEvent {
   reportId: string;
 
+  content: string;
+
   constructor(input: ReportCommentCreatedInput) {
     this.reportId = input.reportId;
+    this.content = input.content;
   }
 }
--- libs/domains/src/report/application/events/report-comment-created/report-comment-created.input.ts
+++ libs/domains/src/report/application/events/report-comment-created/report-comment-created.input.ts
@@ -1,7 +1,10 @@
 export class ReportCommentCreatedInput {
   reportId: string;
 
-  constructor({ reportId }: { reportId: string }) {
+  content: string;
+
+  constructor({ reportId, content }: { reportId: string; content: string }) {
     this.reportId = reportId;
+    this.content = content;
   }
 }
