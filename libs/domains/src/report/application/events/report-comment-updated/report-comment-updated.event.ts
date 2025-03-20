24eb44a8a89575ddb1cd5659cd9d47c8d8a98de2
keypport <keypport@gmail.com>
2025-03-21 01:41:35 +0900

    feat: add reportedUserId in ReportCommentUpdatedEvent

--- libs/domains/src/report/application/events/report-comment-updated/report-comment-updated.event.ts
+++ libs/domains/src/report/application/events/report-comment-updated/report-comment-updated.event.ts
@@ -4,12 +4,15 @@ import { ReportCommentUpdatedInput } from './report-comment-updated.input';
 export class ReportCommentUpdatedEvent implements IEvent {
   reportId: string;
 
+  reportedUserId: string;
+
   oldContent: string;
 
   newContent: string;
 
   constructor(input: ReportCommentUpdatedInput) {
     this.reportId = input.reportId;
+    this.reportedUserId = input.reportedUserId;
     this.oldContent = input.oldContent;
     this.newContent = input.newContent;
   }
--- libs/domains/src/report/application/events/report-comment-updated/report-comment-updated.input.ts
+++ libs/domains/src/report/application/events/report-comment-updated/report-comment-updated.input.ts
@@ -1,20 +1,25 @@
 export class ReportCommentUpdatedInput {
   reportId: string;
 
+  reportedUserId: string;
+
   oldContent: string;
 
   newContent: string;
 
   constructor({
     reportId,
+    reportedUserId,
     oldContent,
     newContent,
   }: {
     reportId: string;
+    reportedUserId: string;
     oldContent: string;
     newContent: string;
   }) {
     this.reportId = reportId;
+    this.reportedUserId = reportedUserId;
     this.oldContent = oldContent;
     this.newContent = newContent;
   }
